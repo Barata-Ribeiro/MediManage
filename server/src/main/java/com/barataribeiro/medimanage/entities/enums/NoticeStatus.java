7e405cefb026f94b1bdd276e1cddee48a88d0c93
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-07-30 05:44:13 -0300

    feat: add Notice entity

---
+++ server/src/main/java/com/barataribeiro/medimanage/entities/enums/NoticeStatus.java
@@ -0,0 +1,6 @@
+package com.barataribeiro.medimanage.entities.enums;
+
+public enum NoticeStatus {
+    ACTIVE,
+    INACTIVE
+}
---
+++ server/src/main/java/com/barataribeiro/medimanage/entities/enums/NoticeType.java
@@ -0,0 +1,8 @@
+package com.barataribeiro.medimanage.entities.enums;
+
+public enum NoticeType {
+    ANNOUNCEMENT,
+    WARNING,
+    ALERT,
+    INFOS
+}
