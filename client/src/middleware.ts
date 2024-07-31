a0acf4e9e791c146cc64824233954fee1b384511
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-07-31 09:46:37 -0300

    refactor: adjust middleware to handle authentication

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,16 +1,31 @@
 import { NextRequest, NextResponse } from "next/server"
+import verifyToken from "@/utils/verify-token"
+import decodeToken from "@/utils/decode-token"
+import { tokenName } from "@/constants"
 
-export function middleware(request: NextRequest) {
-    const url = request.nextUrl.clone()
-    if (url.pathname === "/") {
-        url.pathname = "/c"
-        return NextResponse.redirect(url)
+export async function middleware(request: NextRequest) {
+    const authToken = request.cookies.get(tokenName)?.value
+    const { pathname } = request.nextUrl
+    const clonedUrl = request.nextUrl.clone()
+    const isAuthenticated = authToken ? await verifyToken(authToken) : false
+
+    if (!isAuthenticated && (clonedUrl.pathname === "/" || pathname.startsWith("/dashboard"))) {
+        return NextResponse.redirect(new URL("/auth/login", request.url))
     }
+
+    if (isAuthenticated && (clonedUrl.pathname === "/" || pathname.startsWith("/auth"))) {
+        const jwtPayload = await decodeToken(authToken)
+        return NextResponse.redirect(new URL("/dashboard/" + jwtPayload, request.url))
+    }
+
+    return NextResponse.next()
 }
 
 export const config = {
     matcher: [
         "/((?!_next|[^?]*\\\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
         "/(api|trpc)(.*)",
+        "/dashboard/:path*",
+        "/auth/:path*",
     ],
 }
