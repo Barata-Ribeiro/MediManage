518bcfdc58dd5372f8c8a81f15478967c7161cdb
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-02 06:54:47 -0300

    feat: implement new auth layout style and login page

--- client/src/app/auth/layout.tsx
+++ client/src/app/auth/layout.tsx
@@ -1,9 +1,21 @@
 import { type ReactNode } from "react"
+import authImage from "../../../public/images/auth-image.jpg"
+import Image from "next/image"
 
 export default function AuthLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (
-        <div>
-            <div>{children}</div>
+        <div className="container flex h-screen flex-1">
+            <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+                {children}
+            </div>
+            <div className="relative hidden w-0 flex-1 lg:block">
+                <Image
+                    src={authImage}
+                    alt="Entrance of a medical clinic. Photo by: Erik Mclean on Unsplash"
+                    className="absolute inset-0 h-full w-full object-cover"
+                    priority
+                />
+            </div>
         </div>
     )
 }
--- client/src/components/forms/login-form.tsx
+++ client/src/components/forms/login-form.tsx
@@ -2,6 +2,8 @@
 
 import postAuthLogin from "@/actions/auth/post-auth-login"
 import { useForm } from "@/hooks/use-form"
+import Link from "next/link"
+import RequisitionError from "@/components/helpers/requisition-error"
 import InputValidationError from "@/components/helpers/input-validation-error"
 
 export default function LoginForm() {
@@ -12,15 +14,73 @@ export default function LoginForm() {
     })
 
     return (
-        <form action={formAction} onSubmit={onSubmit}>
-            <input type="text" name="emailOrUsername" placeholder="Username/Email" />
-            <input type="password" name="password" placeholder="Password" />
-            <input type="checkbox" name="rememberMe" />
+        <form action={formAction} onSubmit={onSubmit} className="space-y-6">
+            <div>
+                <label htmlFor="emailOrUsername" className="block text-sm font-medium leading-6 text-neutral-900">
+                    Email or Username
+                </label>
+                <div className="mt-2">
+                    <input
+                        type="text"
+                        id="emailOrUsername"
+                        name="emailOrUsername"
+                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-neutral-400 focus:ring-2 focus:ring-inset focus:ring-mourning-blue-600 sm:text-sm sm:leading-6"
+                        placeholder="Email or Username"
+                        required
+                    />
+                </div>
+            </div>
+
+            <div>
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-neutral-900">
+                    Password
+                </label>
+                <div className="mt-2">
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        placeholder="Password"
+                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-neutral-400 focus:ring-2 focus:ring-inset focus:ring-mourning-blue-600 sm:text-sm sm:leading-6"
+                        required
+                    />
+                </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                    <input
+                        id="remember-me"
+                        name="rememberMe"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-neutral-500 text-hello-spring-600 focus:ring-mourning-blue-600"
+                    />
+                    <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-neutral-700">
+                        Remember me
+                    </label>
+                </div>
+
+                <div className="text-sm leading-6">
+                    <Link
+                        href="/auth/forgot-password"
+                        className="font-semibold text-mourning-blue-600 hover:text-mourning-blue-500">
+                        Forgot password?
+                    </Link>
+                </div>
+            </div>
+
             {formState.error && Array.isArray(formState.error) && <InputValidationError errors={formState.error} />}
-            {formState.error && !Array.isArray(formState.error) && <p>{formState.error.toString()}</p>}
-            <button type="submit" disabled={isPending} className="block text-red-600">
-                {isPending ? "Processing..." : "Login"}
-            </button>
+            {formState.error && !Array.isArray(formState.error) && <RequisitionError error={formState.error} />}
+
+            <div>
+                <button
+                    type="submit"
+                    disabled={isPending}
+                    className="flex w-full justify-center rounded-md bg-mourning-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-mourning-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50">
+                    {isPending ? "Processing..." : "Login"}
+                </button>
+            </div>
         </form>
     )
 }
