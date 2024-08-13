f0b9e04616d8f990478f9281fddedd8857d68f62
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-13 10:18:53 -0300

    feat: add context and info fetching to home page (testing layout only)

--- client/src/app/dashboard/[username]/page.tsx
+++ client/src/app/dashboard/[username]/page.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from "next"
+import getUserContext from "@/actions/users/get-user-context"
+import { User } from "@/interfaces/users"
+import getHomeInfo from "@/actions/get-home-info"
+import { AdministratorInfo, AssistantInfo, DoctorInfo, PatientInfo } from "@/interfaces/home"
 
 export async function generateMetadata({ params }: { params: { username: string } }): Promise<Metadata> {
     return {
@@ -7,33 +11,31 @@ export async function generateMetadata({ params }: { params: { username: string 
     }
 }
 
-export default function Page() {
+export default async function HomePage() {
+    const userState = await getUserContext()
+    const user = userState.response?.data as User
+
+    const homeState = await getHomeInfo(user.accountType)
+    const homeInfo = homeState.response as AdministratorInfo | PatientInfo | AssistantInfo | DoctorInfo
+
+    console.log(homeInfo)
+
     return (
-        <div className="h-full rounded-md bg-neutral-50 p-4 shadow-derek">
-            <p>
-                Iaculisneque usu platonem nullam fuisset purus persequeris animal civibus maiestatis dolorum.
-                Inceptossodales massa quidam nostrum duo quo turpis. Porrotincidunt periculis dui felis iisque fusce
-                tation. Agaminani ignota quas prompta. Nostraconsectetur mattis vim vocent libris ullamcorper mucius
-                oratio fugit.
-            </p>
-            <p>
-                Iaculisneque usu platonem nullam fuisset purus persequeris animal civibus maiestatis dolorum.
-                Inceptossodales massa quidam nostrum duo quo turpis. Porrotincidunt periculis dui felis iisque fusce
-                tation. Agaminani ignota quas prompta. Nostraconsectetur mattis vim vocent libris ullamcorper mucius
-                oratio fugit.
-            </p>
-            <p>
-                Iaculisneque usu platonem nullam fuisset purus persequeris animal civibus maiestatis dolorum.
-                Inceptossodales massa quidam nostrum duo quo turpis. Porrotincidunt periculis dui felis iisque fusce
-                tation. Agaminani ignota quas prompta. Nostraconsectetur mattis vim vocent libris ullamcorper mucius
-                oratio fugit.
-            </p>
-            <p>
-                Iaculisneque usu platonem nullam fuisset purus persequeris animal civibus maiestatis dolorum.
-                Inceptossodales massa quidam nostrum duo quo turpis. Porrotincidunt periculis dui felis iisque fusce
-                tation. Agaminani ignota quas prompta. Nostraconsectetur mattis vim vocent libris ullamcorper mucius
-                oratio fugit.
-            </p>
+        <div className="border-b border-b-gray-900/10 lg:border-t lg:border-t-gray-900/5">
+            <dl className="mx-auto grid max-w-7xl grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 lg:px-2 xl:px-0">
+                {user.accountType === "ASSISTANT" && (
+                    <div className="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 border-t border-gray-900/5 px-4 py-10 sm:px-6 lg:border-t-0 xl:px-8">
+                        <dt className="text-sm font-medium leading-6 text-gray-500">Consultations by Status</dt>
+                        {Object.entries((homeInfo as AssistantInfo).consultationsByStatus).map(([status, count]) => (
+                            <dd
+                                key={status}
+                                className="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">
+                                {status}: {count}
+                            </dd>
+                        ))}
+                    </div>
+                )}
+            </dl>
         </div>
     )
 }
---
+++ client/src/interfaces/home.ts
@@ -0,0 +1,28 @@
+import { Consultation } from "@/interfaces/consultations"
+import { MedicalRecord } from "@/interfaces/records"
+
+interface AdministratorInfo {
+    allUsers: number
+    consultationsByStatus: Record<string, number>
+    totalPrescriptions: number
+}
+
+interface PatientInfo {
+    latestPrescription: object
+    nextConsultation: Consultation | object
+    medicalRecord: MedicalRecord | object
+}
+
+interface AssistantInfo {
+    consultationsByStatus: Record<string, number>
+    todayConsultations: number
+    latestNotice: object
+}
+
+interface DoctorInfo {
+    nextConsultation: Consultation | object
+    consultationsByStatus: number
+    latestNotice: object
+}
+
+export type { AdministratorInfo, PatientInfo, AssistantInfo, DoctorInfo }
