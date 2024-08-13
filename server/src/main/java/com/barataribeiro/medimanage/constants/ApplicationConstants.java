01d87cd126a552c13077b83c131ab07765ebf146
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-13 05:53:20 -0300

    feat: implement new constants from home service

--- server/src/main/java/com/barataribeiro/medimanage/constants/ApplicationConstants.java
+++ server/src/main/java/com/barataribeiro/medimanage/constants/ApplicationConstants.java
@@ -12,6 +12,30 @@ public class ApplicationConstants {
     @NonNls
     public static final String HOME_INFORETRIEVED_SUCCESSFULLY = "Home info retrieved successfully.";
 
+    @NonNls
+    public static final String ALL_USERS = "allUsers";
+
+    @NonNls
+    public static final String CONSULTATIONS_BY_STATUS = "consultationsByStatus";
+
+    @NonNls
+    public static final String TOTAL_PRESCRIPTIONS = "totalPrescriptions";
+
+    @NonNls
+    public static final String LATEST_PRESCRIPTION = "latestPrescription";
+
+    @NonNls
+    public static final String NEXT_CONSULTATION = "nextConsultation";
+
+    @NonNls
+    public static final String MEDICAL_RECORD = "medicalRecord";
+
+    @NonNls
+    public static final String TODAY_CONSULTATIONS = "todayConsultations";
+
+    @NonNls
+    public static final String LATEST_NOTICE = "latestNotice";
+
     private ApplicationConstants() {
         throw new UnsupportedOperationException("This class cannot be instantiated.");
     }
