725b338faafc3d9ac5ae34619969c8600c4731fe
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-14 08:28:40 -0300

    feat: implement new repository method

--- server/src/main/java/com/barataribeiro/medimanage/repositories/CategoryRepository.java
+++ server/src/main/java/com/barataribeiro/medimanage/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.barataribeiro.medimanage.repositories;
 import com.barataribeiro.medimanage.entities.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
