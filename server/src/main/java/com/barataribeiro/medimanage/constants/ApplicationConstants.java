bd899d9aa1c9c86902e73a508172f03f1a2debba
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-14 08:32:35 -0300

    feat: add article request DTO, add default category description

--- server/src/main/java/com/barataribeiro/medimanage/constants/ApplicationConstants.java
+++ server/src/main/java/com/barataribeiro/medimanage/constants/ApplicationConstants.java
@@ -3,17 +3,19 @@ package com.barataribeiro.medimanage.constants;
 import org.jetbrains.annotations.NonNls;
 
 public class ApplicationConstants {
-    @NonNls
-    public static final String CREATED_AT = "createdAt";
-
+    // Messages
     @NonNls
     public static final String USER_REGISTERED_SUCCESSFULLY = "User registered successfully.";
 
     @NonNls
     public static final String HOME_INFORETRIEVED_SUCCESSFULLY = "Home info retrieved successfully.";
 
+    // Attributes
     @NonNls
     public static final String ALL_USERS = "allUsers";
+
+    @NonNls
+    public static final String CREATED_AT = "createdAt";
 
     @NonNls
     public static final String CONSULTATIONS_BY_STATUS = "consultationsByStatus";
@@ -35,6 +37,10 @@ public class ApplicationConstants {
 
     @NonNls
     public static final String LATEST_NOTICE = "latestNotice";
+
+    // Description
+    @NonNls
+    public static final String DEFAULT_CATEGORY_DESCRIPTION = "This is the %s category.";
 
     private ApplicationConstants() {
         throw new UnsupportedOperationException("This class cannot be instantiated.");
