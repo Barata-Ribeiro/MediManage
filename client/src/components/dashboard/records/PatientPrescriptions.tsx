c62b5c9a5388669334b77dead9d44553ec82a10e
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-15 08:16:39 -0300

    feat: implement base component for patient prescription list

---
+++ client/src/components/dashboard/records/PatientPrescriptions.tsx
@@ -0,0 +1,12 @@
+export default function PatientPrescriptions() {
+    return (
+        <div className="w-full px-4 py-6 sm:px-6 lg:px-8">
+            <h2 className="text-base font-semibold leading-7 text-neutral-900">Prescriptions</h2>
+            <div className="mt-6">
+                <p className="relative mx-auto block w-full rounded-lg border border-dashed border-neutral-500 bg-white p-12 text-center text-sm text-neutral-600">
+                    No Prescriptions Found
+                </p>
+            </div>
+        </div>
+    )
+}
