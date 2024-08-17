9487684756f1ab6cb4ca06bbf71ae5803246bdda
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-17 08:30:59 -0300

    feat: implement consultation listing button

--- client/src/components/dashboard/records/patient-consultations.tsx
+++ client/src/components/dashboard/records/patient-consultations.tsx
@@ -1,14 +1,20 @@
 import { PaginatedConsultations } from "@/interfaces/consultations"
 import parseDate from "@/utils/parse-date"
+import { User } from "@/interfaces/users"
+import PatientListConsultationButton from "@/components/dashboard/records/patient-list-consultation-button"
 
 interface PatientConsultationsProps {
     consultationsPage: PaginatedConsultations
+    patient: User
 }
 
-export default function PatientConsultations({ consultationsPage }: Readonly<PatientConsultationsProps>) {
+export default function PatientConsultations({ consultationsPage, patient }: Readonly<PatientConsultationsProps>) {
     return (
         <aside className="hidden w-1/2 border-r border-gray-200 px-4 py-6 sm:px-6 lg:px-8 xl:block">
-            <h2 className="text-base font-semibold leading-7 text-neutral-900">Recent Consultations</h2>
+            <div className="flex flex-wrap justify-between">
+                <h2 className="text-base font-semibold leading-7 text-neutral-900">Recent Consultations</h2>
+                <PatientListConsultationButton name={patient.fullName ?? patient.username} />
+            </div>
             <div className="mt-6 space-y-6">
                 {consultationsPage.content.length > 0 ? (
                     consultationsPage.content.map(consultation => (
@@ -23,43 +29,43 @@ export default function PatientConsultations({ consultationsPage }: Readonly<Pat
                                 switch (consultation.status) {
                                     case "SCHEDULED":
                                         return (
-                                            <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-neutral-600 ring-1 ring-inset ring-neutral-500/10">
+                                            <span className="inline-flex select-none items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-neutral-600 ring-1 ring-inset ring-neutral-500/10">
                                                 Scheduled
                                             </span>
                                         )
                                     case "ACCEPTED":
                                         return (
-                                            <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                                            <span className="inline-flex select-none items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                                                 Accepted
                                             </span>
                                         )
                                     case "IN_PROGRESS":
                                         return (
-                                            <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+                                            <span className="inline-flex select-none items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
                                                 In Progress
                                             </span>
                                         )
                                     case "DONE":
                                         return (
-                                            <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                                            <span className="inline-flex select-none items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                                                 Done
                                             </span>
                                         )
                                     case "MISSED":
                                         return (
-                                            <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                                            <span className="inline-flex select-none items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
                                                 Missed
                                             </span>
                                         )
                                     case "CANCELLED":
                                         return (
-                                            <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                                            <span className="inline-flex select-none items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
                                                 Cancelled
                                             </span>
                                         )
                                     default:
                                         return (
-                                            <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-neutral-600 ring-1 ring-inset ring-neutral-500/10">
+                                            <span className="inline-flex select-none items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-neutral-600 ring-1 ring-inset ring-neutral-500/10">
                                                 Unknown
                                             </span>
                                         )
