3b40c1cde1b89f7b176c837883021481dafb62ba
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-18 12:10:19 -0300

    feat: implement new prescription route

--- client/src/app/dashboard/[username]/records/prescriptions/new/page.tsx
+++ client/src/app/dashboard/[username]/records/prescriptions/new/page.tsx
@@ -1,6 +1,7 @@
 import { RecordsPageProps } from "@/app/dashboard/[username]/records/page"
 import { notFound } from "next/navigation"
 import { Metadata } from "next"
+import NewPrescriptionForm from "@/components/forms/new-prescription-form"
 
 export const metadata: Metadata = {
     title: "New Prescription",
@@ -12,16 +13,21 @@ export default function NewPrescriptionPage({ params, searchParams }: Readonly<R
     if (!searchParams) return null
 
     const userId = searchParams.user as string
-    const requestingUsername = params.username
 
     return (
         <div>
             <div>
                 <h1 className="w-max text-base font-bold leading-6 text-neutral-900">Write a new prescription</h1>
                 <p className="mt-2 max-w-xl text-sm text-neutral-700">
-                    Write a new prescription for a patient. Fill in the form below with the necessary information.
+                    Write a new prescription for a patient. Fill in the field below and click the <strong>issue</strong>{" "}
+                    button to create a new prescription.
+                    <br />
+                    <br />
+                    {/* */}A PDF copy of the prescription will be generated and be made available for download.
                 </p>
             </div>
+
+            <NewPrescriptionForm userId={userId} />
         </div>
     )
 }
