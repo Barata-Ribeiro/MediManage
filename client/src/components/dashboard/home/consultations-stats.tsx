0be84fd8fc6483b22aebdb1e1a1f5085102ccc89
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-20 08:40:16 -0300

    fix: consultation stats component style

--- client/src/components/dashboard/home/consultations-stats.tsx
+++ client/src/components/dashboard/home/consultations-stats.tsx
@@ -11,11 +11,11 @@ export default function ConsultationsStats({ data }: Readonly<DoctorHomeProps>) 
 
     return (
         <div>
-            <h3 className="text-base font-semibold leading-6 text-neutral-900">Consultations</h3>
-            <dl className="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-4 md:divide-x md:divide-y-0">
+            <h3 className="text-lg font-semibold leading-6 text-neutral-900">Consultations</h3>
+            <dl className="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-md bg-white shadow md:grid-cols-4 md:divide-x md:divide-y-0">
                 {filteredData.map(([status, consultations]) => (
                     <div key={status} className="px-4 py-5 sm:p-6">
-                        <dt className="text-base font-normal text-neutral-900">{status}</dt>
+                        <dt className="font-heading text-base font-medium capitalize text-neutral-800">{status}</dt>
                         <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
                             <div className="flex items-baseline text-2xl font-semibold text-mourning-blue-500">
                                 {consultations}
