168c93d4977a7035170418a726b89ff37a76d3c6
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-20 06:46:28 -0300

    refactor: adjust footer text and fix style

--- client/src/components/dashboard/footer.tsx
+++ client/src/components/dashboard/footer.tsx
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 export default function Footer() {
     return (
-        <footer className="container font-body sm:px-6 lg:px-8">
-            <div className="border-t border-neutral-300 py-12 text-center text-neutral-500">
+        <footer className="container font-heading sm:px-6 lg:px-8">
+            <div className="border-t border-neutral-300 py-12 text-center text-neutral-600">
                 <p>
-                    Copyright &copy; {new Date().getFullYear()}. Some rights reserved -{" "}
+                    Copyright &copy; {new Date().getFullYear()}. Some rights reserved. — By
                     <Link
                         href="https://barataribeiro.com/"
-                        className="ml-1 text-neutral-500 hover:text-neutral-600 active:text-neutral-700"
+                        className="ml-1 text-neutral-600 hover:text-neutral-700 active:text-neutral-800"
                         rel="external noopener noreferrer"
                         target="_blank"
                         aria-label="Barata-Ribeiro - Portfolio"
