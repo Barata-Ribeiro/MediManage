f5e1e34ff56b855de5112f6e526d04636c36cec7
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-20 06:42:50 -0300

    feat: add render condition to return skeletons

--- client/src/components/dashboard/home/latest-notice.tsx
+++ client/src/components/dashboard/home/latest-notice.tsx
@@ -1,13 +1,13 @@
 "use client"
 
+import NoticeEmptySkeleton from "@/components/dashboard/skeletons/notice-empty-skeleton"
+
 interface LatestNoticeProps {
     data: object
 }
 
 export default function LatestNotice({ data }: Readonly<LatestNoticeProps>) {
-    if (Object.keys(data).length === 0) {
-        return null
-    }
+    if (Object.keys(data).length === 0) return <NoticeEmptySkeleton />
 
     return <div></div>
 }
--- client/src/components/dashboard/home/next-consultation.tsx
+++ client/src/components/dashboard/home/next-consultation.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import { Consultation } from "@/interfaces/consultations"
+import ConsultationEmptySkeleton from "@/components/dashboard/skeletons/consultation-empty-skeleton"
 
 interface NextConsultationProps {
     data: Consultation | object
 }
 
 export default function NextConsultation({ data }: Readonly<NextConsultationProps>) {
-    if (Object.keys(data).length === 0) return null
+    if (Object.keys(data).length === 0) return <ConsultationEmptySkeleton />
 
     return <div></div>
 }
