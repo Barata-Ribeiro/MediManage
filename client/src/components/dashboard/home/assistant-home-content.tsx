27bc6ece2f0cbd4d2750c39499aba893890dd8a8
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-25 07:57:45 -0300

    feat: implement assistant list for consultations of the day

--- client/src/components/dashboard/home/assistant-home-content.tsx
+++ client/src/components/dashboard/home/assistant-home-content.tsx
@@ -4,11 +4,16 @@ import { FaCircleInfo, FaUserClock } from "react-icons/fa6"
 import LatestNotice from "@/components/dashboard/home/latest-notice"
 import { AssistantInfo } from "@/interfaces/home"
 import TodayConsultationsStats from "@/components/dashboard/home/today-consultations-stats"
+import TodayConsultationsList from "@/components/dashboard/home/today-consultations-list"
 
 export default function AssistantHomeContent({ homeInfo }: Readonly<{ homeInfo: AssistantInfo }>) {
     return (
         <>
-            <TodayConsultationsStats data={homeInfo.todayConsultations} totalData={homeInfo.consultationsByStatus} />
+            <TodayConsultationsStats
+                data={homeInfo.totalConsultationsForToday}
+                totalData={homeInfo.consultationsByStatus}
+            />
+            <TodayConsultationsList data={homeInfo.consultationsForToday} />
             <DividerWithContent>
                 <span className="bg-neutral-50 px-2 text-neutral-600">
                     <FaCircleInfo aria-hidden className="h-5 w-5 text-neutral-600" />
---
+++ client/src/components/dashboard/home/today-consultations-list.tsx
@@ -0,0 +1,85 @@
+"use client"
+
+import { Consultation } from "@/interfaces/consultations"
+import { FaPlus } from "react-icons/fa6"
+import Link from "next/link"
+import { useUser } from "@/context/user-context-provider"
+import ConsultationTableRow from "@/components/dashboard/consultation-table-row"
+
+interface TodayConsultationsListProps {
+    data: Consultation[]
+}
+
+export default function TodayConsultationsList({ data }: Readonly<TodayConsultationsListProps>) {
+    const userData = useUser()
+    return (
+        <div className="rounded-md bg-white px-4 py-5 shadow sm:px-6">
+            <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
+                <div className="ml-4 mt-2">
+                    <h3 className="text-base font-semibold leading-6 text-neutral-900">List</h3>
+                </div>
+                <div className="ml-4 mt-2 flex-shrink-0">
+                    <Link
+                        href={`/dashboard/${userData.user?.username}/consultations/schedule`}
+                        className="order-2 inline-flex w-max items-center gap-2 rounded-md bg-mourning-blue-600 px-3 py-2 text-center font-heading text-sm font-semibold text-white shadow-sm hover:bg-mourning-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-mourning-blue-600 active:bg-mourning-blue-800 sm:order-1 sm:justify-self-end">
+                        New Consultation <FaPlus className="inline-block" />
+                    </Link>
+                </div>
+            </div>
+            <div className="mt-4">
+                <table className="min-w-full divide-y divide-neutral-200">
+                    <thead className="bg-neutral-100">
+                        <tr>
+                            <th
+                                scope="col"
+                                className="px-6 py-3 text-left font-heading text-xs font-semibold uppercase tracking-wider text-neutral-600">
+                                ID
+                            </th>
+                            <th
+                                scope="col"
+                                className="px-6 py-3 text-left font-heading text-xs font-semibold uppercase tracking-wider text-neutral-600">
+                                Patient
+                            </th>
+                            <th
+                                scope="col"
+                                className="hidden px-6 py-3 text-left font-heading text-xs font-semibold uppercase tracking-wider text-neutral-600 lg:table-cell">
+                                Doctor
+                            </th>
+                            <th
+                                scope="col"
+                                className="w-max px-6 py-3 text-left font-heading text-xs font-semibold uppercase tracking-wider text-neutral-600">
+                                Scheduled To
+                            </th>
+                            <th
+                                scope="col"
+                                className="px-6 py-3 text-left font-heading text-xs font-semibold uppercase tracking-wider text-neutral-600">
+                                Status
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.length > 0 &&
+                            data.map((consult, consultIdx) => (
+                                <ConsultationTableRow
+                                    key={consult.id}
+                                    index={consultIdx}
+                                    length={data.length}
+                                    consult={consult}
+                                />
+                            ))}
+
+                        {data.length < 1 && (
+                            <tr className="border-b border-neutral-300 bg-white">
+                                <td
+                                    colSpan={6}
+                                    className="py-4 pl-4 pr-3 text-sm font-bold text-neutral-900 sm:pl-6 lg:pl-8">
+                                    No consultations for today.
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
