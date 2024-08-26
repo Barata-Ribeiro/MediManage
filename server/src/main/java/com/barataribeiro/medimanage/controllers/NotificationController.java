b775d5b8cc9f769dbbd67bd811cecc4c069dd3a1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-26 06:55:02 -0300

    feat: implement notifications controller

---
+++ server/src/main/java/com/barataribeiro/medimanage/controllers/NotificationController.java
@@ -0,0 +1,76 @@
+package com.barataribeiro.medimanage.controllers;
+
+import com.barataribeiro.medimanage.dtos.raw.NotificationDTO;
+import com.barataribeiro.medimanage.dtos.raw.RestResponseDTO;
+import com.barataribeiro.medimanage.services.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/notifications")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<RestResponseDTO> getAllUserNotifications(@PathVariable String userId,
+                                                                   @RequestParam(required = false) Boolean isRead,
+                                                                   @RequestParam(defaultValue = "0") int page,
+                                                                   @RequestParam(defaultValue = "10") int perPage,
+                                                                   @RequestParam(defaultValue = "ASC") String direction,
+                                                                   @RequestParam(defaultValue = "issuedAt")
+                                                                   String orderBy) {
+        Page<NotificationDTO> response = notificationService.getAllUserNotifications(userId, isRead, page, perPage,
+                                                                                     direction, orderBy);
+        return ResponseEntity.ok(new RestResponseDTO(HttpStatus.OK,
+                                                     HttpStatus.OK.value(),
+                                                     "Notifications retrieved successfully.",
+                                                     response));
+    }
+
+    @GetMapping("/{userId}/latest")
+    public ResponseEntity<RestResponseDTO> getLatestUserNotifications(@PathVariable String userId) {
+        List<NotificationDTO> response = notificationService.getLatestUserNotifications(userId);
+        return ResponseEntity.ok(new RestResponseDTO(HttpStatus.OK,
+                                                     HttpStatus.OK.value(),
+                                                     "Notifications retrieved successfully.",
+                                                     response));
+    }
+
+    @GetMapping("/{userId}/{notificationId}")
+    public ResponseEntity<RestResponseDTO> getNotification(@PathVariable String userId,
+                                                           @PathVariable String notificationId) {
+        NotificationDTO response = notificationService.getNotification(userId, notificationId);
+        return ResponseEntity.ok(new RestResponseDTO(HttpStatus.OK,
+                                                     HttpStatus.OK.value(),
+                                                     "Notification retrieved successfully.",
+                                                     response));
+    }
+
+    @PatchMapping("/{userId}/{notificationId}")
+    public ResponseEntity<RestResponseDTO> changeNotificationStatus(@PathVariable String userId,
+                                                                    @PathVariable String notificationId,
+                                                                    @RequestParam Boolean isRead) {
+        NotificationDTO response = notificationService.changeNotificationStatus(userId, notificationId, isRead);
+        return ResponseEntity.ok(new RestResponseDTO(HttpStatus.OK,
+                                                     HttpStatus.OK.value(),
+                                                     "Notification status changed successfully.",
+                                                     response));
+    }
+
+    @DeleteMapping("/{userId}/{notificationId}")
+    public ResponseEntity<RestResponseDTO> deleteNotification(@PathVariable String userId,
+                                                              @PathVariable String notificationId) {
+        notificationService.deleteNotification(userId, notificationId);
+        return ResponseEntity.ok(new RestResponseDTO(HttpStatus.OK,
+                                                     HttpStatus.OK.value(),
+                                                     "Notification deleted successfully.",
+                                                     null));
+    }
+}
