816593e072d194bab2631aad25e2ce9acef0814a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-26 08:55:32 -0300

    feat: implement get routes for notifications

--- server/src/main/java/com/barataribeiro/medimanage/repositories/NotificationRepository.java
+++ server/src/main/java/com/barataribeiro/medimanage/repositories/NotificationRepository.java
@@ -1,7 +1,33 @@
 package com.barataribeiro.medimanage.repositories;
 
 import com.barataribeiro.medimanage.entities.models.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    @EntityGraph(attributePaths = {"user"})
+    @Query("""
+           SELECT n FROM Notification n
+           LEFT JOIN FETCH n.user u
+           WHERE u.id = :userId AND n.isRead = :isRead
+           """)
+    Page<Notification> findAllNotificationsPaginated(@Param("isRead") Boolean isRead, @Param("userId") String userId,
+                                                     Pageable pageable);
+
+    @EntityGraph(attributePaths = {"user"})
+    Page<Notification> findAllByUser_Id(UUID id, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"user"})
+    Optional<Notification> findFirstByIdAndUser_Id(Long notifId, UUID userId);
+
+    @EntityGraph(attributePaths = {"user"})
+    List<Notification> findDistinctTop5ByUser_IdOrderByIssuedAtAsc(UUID id);
 }
--- server/src/main/java/com/barataribeiro/medimanage/services/impl/NotificationServiceImpl.java
+++ server/src/main/java/com/barataribeiro/medimanage/services/impl/NotificationServiceImpl.java
@@ -1,35 +1,66 @@
 package com.barataribeiro.medimanage.services.impl;
 
+import com.barataribeiro.medimanage.builders.NotificationMapper;
+import com.barataribeiro.medimanage.constants.ApplicationConstants;
+import com.barataribeiro.medimanage.constants.ApplicationMessages;
 import com.barataribeiro.medimanage.dtos.raw.NotificationDTO;
+import com.barataribeiro.medimanage.entities.models.Notification;
+import com.barataribeiro.medimanage.exceptions.notifications.NotificationNotFoundException;
+import com.barataribeiro.medimanage.repositories.NotificationRepository;
 import com.barataribeiro.medimanage.services.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class NotificationServiceImpl implements NotificationService {
+    private final NotificationRepository notificationRepository;
+    private final NotificationMapper notificationMapper;
+
     @Override
     @Transactional(readOnly = true)
     public Page<NotificationDTO> getAllUserNotifications(String userId, Boolean isRead, int page, int perPage,
-                                                         String direction, String orderBy) {
-        return null;
+                                                         @NotNull String direction, String orderBy) {
+        Sort.Direction sortDirection = direction.equalsIgnoreCase("DESC") ? Sort.Direction.DESC : Sort.Direction.ASC;
+        orderBy = orderBy.equalsIgnoreCase(ApplicationConstants.ISSUED_AT) ? ApplicationConstants.ISSUED_AT : orderBy;
+        PageRequest pageable = PageRequest.of(page, perPage, Sort.by(sortDirection, orderBy));
+
+        Page<Notification> notifications;
+        if (isRead == null) notifications = notificationRepository.findAllByUser_Id(UUID.fromString(userId), pageable);
+        else notifications = notificationRepository.findAllNotificationsPaginated(isRead, userId, pageable);
+
+        List<NotificationDTO> notificationDTOS = notificationMapper.toDTOList(notifications.getContent());
+
+        return new PageImpl<>(notificationDTOS, pageable, notifications.getTotalElements());
     }
 
     @Override
     @Transactional(readOnly = true)
     public NotificationDTO getNotification(String userId, String notificationId) {
-        return null;
+        Notification notification = notificationRepository.findFirstByIdAndUser_Id(Long.parseLong(notificationId),
+                                                                                   UUID.fromString(userId))
+                .orElseThrow(() -> new NotificationNotFoundException(
+                        String.format(ApplicationMessages.NOTIFICATION_NOT_FOUND_WITH_ID, notificationId)
+                ));
+        return notificationMapper.toDTO(notification);
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<NotificationDTO> getLatestUserNotifications(String userId) {
-        return List.of();
+        List<Notification> notifications = notificationRepository.findDistinctTop5ByUser_IdOrderByIssuedAtAsc(
+                UUID.fromString(userId));
+        return notificationMapper.toDTOList(notifications);
     }
 
     @Override
