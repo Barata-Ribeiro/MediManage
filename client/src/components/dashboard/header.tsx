7be0f8edaaf0915e54fac89e69da86099a200d05
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-29 06:58:58 -0300

    refactor: adjust header styles and burger menu

--- client/src/components/dashboard/header.tsx
+++ client/src/components/dashboard/header.tsx
@@ -7,13 +7,16 @@ import {
     DisclosurePanel,
     Menu,
     MenuButton,
+    MenuHeading,
     MenuItem,
     MenuItems,
+    MenuSection,
+    MenuSeparator,
     Transition,
 } from "@headlessui/react"
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 import { twMerge } from "tailwind-merge"
-import { FaBars, FaUserDoctor, FaUserInjured, FaUserNurse, FaUserTie, FaX } from "react-icons/fa6"
+import { FaBars, FaBell, FaUserDoctor, FaUserInjured, FaUserNurse, FaUserTie, FaX } from "react-icons/fa6"
 import { usePathname } from "next/navigation"
 import Link from "next/link"
 import miniLogo from "../../../public/images/medimanage-mini.svg"
@@ -23,6 +26,14 @@ import NotificationButton from "@/components/dashboard/notification-button"
 export default function Header() {
     const data = useUser()
     const pathname = usePathname()
+
+    const [isNotifDisabled, setIsNotifDisabled] = useState(!data)
+    if (!data) setIsNotifDisabled(true)
+
+    const totalUnreadNotifications =
+        data.user?.totalUnreadNotifications === undefined ? "X" : data.user.totalUnreadNotifications
+    const displayNotifications =
+        typeof totalUnreadNotifications === "number" && totalUnreadNotifications > 99 ? "99+" : totalUnreadNotifications
 
     const encodedName = encodeURIComponent(data.user?.username ?? "")
     const userIsAdmin = data.user?.accountType === "ADMINISTRATOR"
@@ -120,20 +131,33 @@ export default function Header() {
                                                     leaveFrom="transform opacity-100 scale-100"
                                                     leaveTo="transform opacity-0 scale-95">
                                                     <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-neutral-50 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                        {userNavigation.map(item => (
-                                                            <MenuItem key={item.name}>
-                                                                {({ focus }) => (
-                                                                    <Link
-                                                                        href={item.href}
-                                                                        className={twMerge(
-                                                                            focus ? "bg-mourning-blue-100" : "",
-                                                                            "block px-4 py-2 text-sm text-neutral-700",
-                                                                        )}>
-                                                                        {item.name}
-                                                                    </Link>
-                                                                )}
-                                                            </MenuItem>
-                                                        ))}
+                                                        <MenuSection className="grid gap-1 px-4 py-2 font-heading">
+                                                            <MenuHeading className="text-base font-medium leading-none">
+                                                                {data.user?.username}
+                                                            </MenuHeading>
+                                                            <p className="text-sm leading-none text-neutral-500">
+                                                                {data.user?.email}
+                                                            </p>
+                                                        </MenuSection>
+
+                                                        <MenuSeparator className="my-1 h-px bg-neutral-200" />
+
+                                                        <MenuSection>
+                                                            {userNavigation.map(item => (
+                                                                <MenuItem key={item.name}>
+                                                                    {({ focus }) => (
+                                                                        <Link
+                                                                            href={item.href}
+                                                                            className={twMerge(
+                                                                                focus ? "bg-mourning-blue-100" : "",
+                                                                                "block px-4 py-2 text-sm text-neutral-700",
+                                                                            )}>
+                                                                            {item.name}
+                                                                        </Link>
+                                                                    )}
+                                                                </MenuItem>
+                                                            ))}
+                                                        </MenuSection>
                                                     </MenuItems>
                                                 </Transition>
                                             </Menu>
@@ -177,19 +201,37 @@ export default function Header() {
                             <div className="border-t border-neutral-700 pb-3 pt-4">
                                 <div className="flex items-center px-5">
                                     <div className="flex-shrink-0">
-                                        <span id="avatar" className="h-10 w-10 rounded-full leading-none">
+                                        <span
+                                            id="avatar"
+                                            className="h-10 w-10 rounded-full leading-none text-neutral-50">
                                             {getUserIcon(userIsAdmin, userIsDoctor, userIsAssistant)}
                                         </span>
                                     </div>
-                                    <div className="ml-3">
-                                        <div className="text-base font-medium leading-none text-neutral-50">
+                                    <p className="ml-3 grid gap-1">
+                                        <span className="text-base font-medium leading-none text-neutral-50">
                                             {data.user?.username}
-                                        </div>
-                                        <div className="text-sm font-medium leading-none text-neutral-400">
+                                        </span>
+                                        <span className="text-sm font-medium leading-none text-neutral-400">
                                             {data.user?.email}
+                                        </span>
+                                    </p>
+                                    <div className="relative ml-3 inline-flex w-fit">
+                                        <div
+                                            className={twMerge(
+                                                "absolute bottom-auto left-0 right-auto top-0 z-10 inline-block -translate-y-1/3 translate-x-1/2 rotate-0 skew-x-0 skew-y-0 scale-x-75 scale-y-75 select-none whitespace-nowrap rounded-full px-2.5 py-1 text-center align-baseline text-xs font-bold leading-none text-white",
+                                                isNotifDisabled || totalUnreadNotifications === 0
+                                                    ? "bg-neutral-600"
+                                                    : "bg-hello-spring-600",
+                                            )}>
+                                            {displayNotifications}
                                         </div>
+                                        <Link
+                                            href={"/dashboard/" + encodedName + "/notifications"}
+                                            className="rounded-full bg-mourning-blue-800 p-1 text-neutral-400 hover:text-neutral-50 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 disabled:cursor-default disabled:opacity-50">
+                                            <span className="sr-only">View notifications</span>
+                                            <FaBell className="h-6 w-6" aria-hidden />
+                                        </Link>
                                     </div>
-                                    <NotificationButton />
                                 </div>
                                 <div className="mt-3 space-y-1 px-2">
                                     {userNavigation.map(item => (
