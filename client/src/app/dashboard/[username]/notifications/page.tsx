71803f68ae9edc96a48cb15d4ce5d45cdab8eb22
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-08-30 07:14:01 -0300

    feat: implement base notif listing page with in-page modal

---
+++ client/src/app/dashboard/[username]/notifications/page.tsx
@@ -0,0 +1,148 @@
+import { notFound } from "next/navigation"
+import getAllUserNotifications from "@/actions/notifications/get-all-user-notifications"
+import getUserContext from "@/actions/users/get-user-context"
+import StateError from "@/components/helpers/state-error"
+import { ProblemDetails } from "@/interfaces/actions"
+import { User } from "@/interfaces/users"
+import { PaginatedNotifications } from "@/interfaces/notifications"
+import NavigationPagination from "@/components/dashboard/filters/navigation-pagination"
+import { twMerge } from "tailwind-merge"
+import { IoMailOpen, IoMailUnread } from "react-icons/io5"
+import type { Metadata } from "next"
+import parseDate from "@/utils/parse-date"
+import NotificationMessageModal from "@/components/modals/notification-message-modal"
+
+interface NotificationsPageProps {
+    params: { username: string }
+    searchParams?: { [key: string]: string | string[] | undefined }
+}
+
+export const metadata: Metadata = {
+    title: "Your notifications",
+    description: "View all your notifications here in one place.",
+}
+
+export default async function NotificationsPage({ params, searchParams }: Readonly<NotificationsPageProps>) {
+    if (!params.username) return notFound()
+    if (!searchParams) return null
+
+    const page = parseInt(searchParams.page as string, 10) || 0
+    const perPage = parseInt(searchParams.perPage as string, 10) || 10
+    const direction = (searchParams.direction as string) || "DESC"
+    const orderBy = (searchParams.orderBy as string) || "issuedAt"
+
+    const contextState = await getUserContext()
+    if (!contextState.ok) return <StateError error={contextState.error as ProblemDetails} />
+    const user = contextState.response?.data as User
+
+    const notifState = await getAllUserNotifications(user.id, page, perPage, direction, orderBy)
+    if (!notifState.ok) return <StateError error={notifState.error as ProblemDetails} />
+
+    const pagination = notifState.response?.data as PaginatedNotifications
+    const content = pagination.content
+    const pageInfo = pagination.page
+
+    return (
+        <section
+            id="notification-section"
+            aria-labelledby="notification-section-title"
+            className="px-4 sm:px-6 lg:px-8">
+            <div className="flex flex-wrap items-center gap-4 md:justify-between">
+                <div className="w-max flex-auto">
+                    <h1
+                        id="notification-section-title"
+                        className="w-max text-base font-bold leading-6 text-neutral-900">
+                        Notifications
+                    </h1>
+                    <p className="mt-2 max-w-xl text-sm text-neutral-700">
+                        View all your notifications here in one place. You can mark them as read or unread. Or you can
+                        delete them if you don&apos;t need them anymore.
+                    </p>
+
+                    {/*IMPLEMENT SORTING*/}
+                </div>
+            </div>
+            <div className="mt-8 flow-root">
+                <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 align-middle">
+                        <table className="min-w-full border-separate border-spacing-0">
+                            <thead>
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="sticky top-0 z-10 border-b border-neutral-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left font-heading text-sm font-semibold text-neutral-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
+                                        <span className="sr-only">
+                                            Check multiple notifications at once by selecting the checkbox
+                                        </span>
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="sticky top-0 z-10 border-b border-neutral-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left font-heading text-sm font-semibold text-neutral-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8">
+                                        Id
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="sticky top-0 z-10 hidden border-b border-neutral-300 bg-white bg-opacity-75 px-3 py-3.5 text-left font-heading text-sm font-semibold text-neutral-900 backdrop-blur backdrop-filter sm:table-cell">
+                                        Title
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="sticky top-0 z-10 hidden border-b border-neutral-300 bg-white bg-opacity-75 px-3 py-3.5 text-left font-heading text-sm font-semibold text-neutral-900 backdrop-blur backdrop-filter sm:table-cell">
+                                        Issued At
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="sticky top-0 z-10 hidden border-b border-neutral-300 bg-white bg-opacity-75 px-3 py-3.5 text-left font-heading text-sm font-semibold text-neutral-900 backdrop-blur backdrop-filter sm:table-cell">
+                                        Manage
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {content.length > 0 &&
+                                    content.map((notif, notifIdx) => {
+                                        const isRead = notif.isRead
+                                        const isLast = notifIdx === content.length - 1
+                                        return (
+                                            <tr
+                                                key={notif.id + "_" + notifIdx}
+                                                className={twMerge(isRead ? "bg-neutral-50" : "bg-white")}>
+                                                <td></td>
+                                                <td
+                                                    className={twMerge(
+                                                        !isLast ? "border-b border-neutral-200" : "",
+                                                        "whitespace-nowrap py-4 pl-4 pr-3 font-body text-sm font-medium text-neutral-900 sm:pl-6 lg:pl-8",
+                                                    )}>
+                                                    {notif.id}
+                                                </td>
+                                                <td
+                                                    className={twMerge(
+                                                        !isLast ? "border-b border-neutral-200" : "",
+                                                        isRead ? "font-normal" : "font-medium",
+                                                        "flex gap-2 whitespace-nowrap px-3 py-4 font-heading text-sm text-neutral-900",
+                                                    )}>
+                                                    {isRead ? (
+                                                        <IoMailOpen className="h-5 w-5" aria-hidden title="Read" />
+                                                    ) : (
+                                                        <IoMailUnread className="h-5 w-5" aria-hidden title="Unread" />
+                                                    )}
+                                                    <NotificationMessageModal notif={notif} />
+                                                </td>
+                                                <td
+                                                    className={twMerge(
+                                                        !isLast ? "border-b border-neutral-200" : "",
+                                                        "whitespace-nowrap px-3 py-4 font-body text-sm text-neutral-900",
+                                                    )}>
+                                                    {parseDate(notif.issuedAt)}
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                            </tbody>
+                        </table>
+                        <NavigationPagination usePageInfo={pageInfo} contentSize={content.length} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ client/src/components/modals/notification-message-modal.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { useUser } from "@/context/user-context-provider"
+import { Button, Description, Dialog, DialogBackdrop, DialogPanel, DialogTitle } from "@headlessui/react"
+import { useState } from "react"
+import { Notification } from "@/interfaces/notifications"
+import { IoMail } from "react-icons/io5"
+
+interface NotificationMessageProps {
+    notif: Notification
+}
+
+export default function NotificationMessageModal({ notif }: Readonly<NotificationMessageProps>) {
+    const [open, setOpen] = useState(false)
+    const { user } = useUser()
+
+    return (
+        <>
+            <Button type="button" onClick={() => setOpen(true)}>
+                {notif.title}
+            </Button>
+            <Dialog open={open} onClose={setOpen} className="relative z-10">
+                <DialogBackdrop
+                    transition
+                    className="fixed inset-0 bg-neutral-800 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+                />
+
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <DialogPanel
+                            transition
+                            className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95">
+                            <div>
+                                <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-mourning-blue-100">
+                                    <IoMail aria-hidden className="h-6 w-6 text-mourning-blue-300" />
+                                </div>
+                                <div className="mt-3 text-center sm:mt-5">
+                                    <DialogTitle
+                                        as="h3"
+                                        className="font-heading text-lg font-semibold leading-6 text-neutral-900">
+                                        {notif.title}
+                                    </DialogTitle>
+                                    <Description className="prose mt-2 text-base text-neutral-700">
+                                        {notif.message}
+                                    </Description>
+                                </div>
+                            </div>
+                            <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                                <Button
+                                    type="button"
+                                    onClick={() => setOpen(false)}
+                                    className="inline-flex w-full justify-center rounded-md bg-mourning-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-mourning-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-mourning-blue-600 active:bg-mourning-blue-800 sm:col-start-2">
+                                    Mark as Read
+                                </Button>
+                                <Button
+                                    type="button"
+                                    data-autofocus={true}
+                                    onClick={() => setOpen(false)}
+                                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-neutral-900 shadow-sm ring-1 ring-inset ring-neutral-300 hover:bg-neutral-50 active:bg-neutral-200 sm:col-start-1 sm:mt-0">
+                                    Close
+                                </Button>
+                            </div>
+                        </DialogPanel>
+                    </div>
+                </div>
+            </Dialog>
+        </>
+    )
+}
