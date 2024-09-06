15afdae7b98f9f7f05d2cf3d93a0cf77aa8876b8
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-09-06 06:28:17 -0300

    refactor: add list all link to retrieve patient's prescriptions

--- client/src/components/dashboard/records/patient-prescriptions.tsx
+++ client/src/components/dashboard/records/patient-prescriptions.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import { PaginatedSimplePrescriptions } from "@/interfaces/prescriptions"
 import parseDate from "@/utils/parse-date"
 import Link from "next/link"
 import PatientNewPrescriptionButton from "@/components/dashboard/records/patient-new-prescription-button"
 import { User } from "@/interfaces/users"
+import { useUser } from "@/context/user-context-provider"
 
 interface PatientPrescriptionsProps {
     prescriptionsPage: PaginatedSimplePrescriptions
@@ -10,18 +13,27 @@ interface PatientPrescriptionsProps {
 }
 
 export default function PatientPrescriptions({ prescriptionsPage, patient }: Readonly<PatientPrescriptionsProps>) {
+    const userData = useUser()
+    const baseUrl = "/dashboard/" + userData.user?.username + "/records/prescriptions"
     return (
         <div className="w-full px-4 py-6 sm:px-6 lg:px-8">
             <div className="flex flex-wrap justify-between gap-2">
                 <h2 className="text-base font-semibold leading-7 text-neutral-900">Recent Prescriptions</h2>
-                <PatientNewPrescriptionButton userId={patient.id} />
+                <div className="flex flex-row-reverse items-center gap-2">
+                    <PatientNewPrescriptionButton userId={patient.id} />
+                    <Link
+                        href={baseUrl + "?user=" + patient.id}
+                        className="font-heading font-medium text-mourning-blue-600 hover:text-mourning-blue-700 active:text-mourning-blue-800">
+                        List all
+                    </Link>
+                </div>
             </div>
             <ul className="mt-6 grid gap-4">
                 {prescriptionsPage.content.length > 0 ? (
                     prescriptionsPage.content.map(prescription => (
                         <li
                             key={prescription.id}
-                            className="flex items-center justify-between rounded-md border bg-white p-2 hover:bg-neutral-100">
+                            className="flex flex-wrap items-center justify-between gap-2 rounded-md border bg-white p-2 hover:bg-neutral-100 sm:gap-0">
                             <div>
                                 <h3 className="text-sm font-medium text-neutral-900">{prescription.doctor.fullName}</h3>
                                 <p className="text-sm text-neutral-600">
