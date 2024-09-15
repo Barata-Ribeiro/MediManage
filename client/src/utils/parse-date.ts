b0d45fea60ebb25b2ccf55b058f66dbbd878d37a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-09-15 07:28:34 -0300

    fix: adjust function to parse date correctly

--- client/src/utils/parse-date.ts
+++ client/src/utils/parse-date.ts
@@ -1,8 +1,11 @@
 export default function parseDate(date?: string | null): string {
     if (!date) return "Unknown"
 
-    if (date.match(/^\d{4}-\d{2}-\d{2}$/)) {
-        return new Date(date).toLocaleDateString("en-US", {
+    if (/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+        const [year, month, day] = date.split("-").map(Number)
+        const localDate = new Date(year, month - 1, day)
+
+        return localDate.toLocaleDateString("en-US", {
             year: "numeric",
             month: "long",
             day: "numeric",
