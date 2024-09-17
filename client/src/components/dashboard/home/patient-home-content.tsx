3a63be018e9074ec2914cac8c5bbcafeacfb2e80
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-09-17 10:31:50 -0300

    refactor: add next consult component to patient home

--- client/src/components/dashboard/home/patient-home-content.tsx
+++ client/src/components/dashboard/home/patient-home-content.tsx
@@ -2,10 +2,11 @@
 
 import { PatientInfo } from "@/interfaces/home"
 import LatestNotice from "@/components/dashboard/home/latest-notice"
-import { FaCircleInfo } from "react-icons/fa6"
+import { FaCircleInfo, FaUserClock } from "react-icons/fa6"
 import DividerWithContent from "@/components/helpers/divider-with-content"
 import { useUser } from "@/context/user-context-provider"
 import MedicalRecordAndPrescription from "@/components/dashboard/home/medical-record-and-prescription"
+import NextConsultation from "@/components/dashboard/home/next-consultation"
 
 export default function PatientHomeContent({ homeInfo }: Readonly<{ homeInfo: PatientInfo }>) {
     const userData = useUser()
@@ -20,7 +21,13 @@ export default function PatientHomeContent({ homeInfo }: Readonly<{ homeInfo: Pa
             />
             <DividerWithContent>
                 <span className="bg-neutral-50 px-2 text-neutral-600">
-                    <FaCircleInfo aria-hidden className="h-5 w-5 text-neutral-600" />
+                    <FaUserClock aria-hidden className="h-5 w-5 text-neutral-600" />
+                </span>
+            </DividerWithContent>
+            <NextConsultation data={homeInfo.nextConsultation} />
+            <DividerWithContent>
+                <span className="bg-neutral-50 px-2 text-neutral-600">
+                    <FaCircleInfo aria-hidden className="h-5 w-5 bg-neutral-50 text-neutral-600" />
                 </span>
             </DividerWithContent>
             <LatestNotice data={homeInfo.latestNotice} />
