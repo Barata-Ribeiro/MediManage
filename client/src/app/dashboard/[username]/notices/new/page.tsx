33a56466ef58e2f248440ce3172358af24ce0963
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-09-22 11:28:00 -0300

    feat: implement base page for view and create new notice

---
+++ client/src/app/dashboard/[username]/notices/new/page.tsx
@@ -0,0 +1,15 @@
+export default function NewNotice() {
+    return (
+        <section id="new-notice-section" aria-labelledby="new-notice-section-title" className="h-[30rem]">
+            <div>
+                <h1 id="new-notice-section-title" className="w-max text-base font-bold leading-6 text-neutral-900">
+                    Create a new notice
+                </h1>
+                <p className="mt-2 max-w-xl text-sm text-neutral-700">
+                    Create a new notice to be displayed on the home dashboard, or, if available, on the public board for
+                    the clinic&apos;s website. Fill in the form below with the necessary information.
+                </p>
+            </div>
+        </section>
+    )
+}
