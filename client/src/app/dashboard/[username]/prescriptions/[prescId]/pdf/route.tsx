6a5866bdf25c781295469c7ee1f48f51666c6f9e
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-09-27 06:25:48 -0300

    chore, fix: move route to correct package, use correct server action

--- client/src/app/dashboard/[username]/prescriptions/[prescId]/pdf/route.tsx
+++ client/src/app/dashboard/[username]/prescriptions/[prescId]/pdf/route.tsx
@@ -1,9 +1,8 @@
-import getPrescriptionByIdAndUsername from "@/actions/prescriptions/get-prescription-by-id-and-username"
-import { notFound } from "next/navigation"
 import { renderToStream } from "@react-pdf/renderer"
 import { NextResponse } from "next/server"
 import PrescriptionPdf from "@/components/prescription-pdf"
 import { Prescription } from "@/interfaces/prescriptions"
+import getMyPrescriptionById from "@/actions/prescriptions/get-my-prescription-by-id"
 
 interface PrescriptionPageProps {
     params: { username: string; prescId: string }
@@ -13,8 +12,8 @@ export async function GET(req: Request, { params }: PrescriptionPageProps) {
     if (!params.username || !params.prescId) throw new Error("Missing params.")
     if (isNaN(parseInt(params.prescId))) throw new Error("Invalid prescription Id")
 
-    const state = await getPrescriptionByIdAndUsername({ id: params.prescId, username: params.username })
-    if (!state.ok || state.error) notFound()
+    const state = await getMyPrescriptionById({ id: params.prescId })
+    if (!state.ok || state.error) return NextResponse.json(state.error)
     const prescription = state.response?.data as Prescription
 
     const stream = await renderToStream(<PrescriptionPdf data={prescription} />)
