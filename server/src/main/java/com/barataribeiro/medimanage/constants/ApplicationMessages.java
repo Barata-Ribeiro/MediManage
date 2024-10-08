ff1996d138e3a6ad14ab1078bbaaab29a7ae7a7e
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-10-08 06:08:38 -0300

    remove: delete unused app messages

--- server/src/main/java/com/barataribeiro/medimanage/constants/ApplicationMessages.java
+++ server/src/main/java/com/barataribeiro/medimanage/constants/ApplicationMessages.java
@@ -2,16 +2,8 @@ package com.barataribeiro.medimanage.constants;
 
 public final class ApplicationMessages {
 
-    public static final String ARTICLE_NOT_FOUND_WITH_ID = "Article with ID %s not found.";
-    public static final String CONSULTATION_NOT_FOUND_WITH_ID = "Consultation with ID %s not found.";
+
     public static final String KEEP_ALIVE_FOR = "Keep alive for %s minutes.";
-    public static final String MEDICAL_RECORD_NOT_FOUND_WITH_ID = "Medical record with ID %s not found.";
-    public static final String MEDICAL_RECORD_NOT_FOUND_WITH_NAME = "Medical record with name %s not found.";
-    public static final String NOTICE_NOT_FOUND_WITH_ID = "Notice with ID %s not found.";
-    public static final String NOTIFICATION_NOT_FOUND_WITH_ID = "Notification with ID %s not found.";
-    public static final String PRESCRIPTION_NOT_FOUND_WITH_ID = "Prescription with ID %s not found.";
-    public static final String USER_NOT_FOUND_WITH_ID = "User with ID %s not found.";
-    public static final String USER_NOT_FOUND_WITH_NAME = "User with name %s not found.";
     public static final String USER_NOT_FOUND_WITH_USERNAME = "User with username %s not found.";
 
     private ApplicationMessages() {
