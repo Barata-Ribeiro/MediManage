4a9ea46ed7b28fc5e32e5e98eca024e6a94beba4
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-11-30 10:38:40 -0300

    feat(tailwind): update Tailwind CSS configuration
    - Added new boxShadow utilities: `standard`, `brutal`, and `nextJS`.
    - Included `safelist` pattern for dynamic class generation.

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -45,6 +45,8 @@ const config: Config = {
                 },
             },
             boxShadow: {
+                standard:
+                    "0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08);",
                 derek:
                     "0px 0px 0px 1px rgb(0 0 0 / 0.06),\n" +
                     "0px 1px 1px -0.5px rgb(0 0 0 / 0.06),\n" +
@@ -52,9 +54,12 @@ const config: Config = {
                     "0px 6px 6px -3px rgb(0 0 0 / 0.06),\n" +
                     "0px 12px 12px -6px rgb(0 0 0 / 0.06),\n" +
                     "0px 24px 24px -12px rgb(0 0 0 / 0.06);",
+                brutal: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;",
+                nextJS: "0 8px 30px rgb(0,0,0,0.12);",
             },
         },
     },
+    safelist: [{ pattern: /(bg|text|border|fill)-./ }],
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 }
