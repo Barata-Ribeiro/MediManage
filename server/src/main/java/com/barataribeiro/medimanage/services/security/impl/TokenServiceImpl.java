27bcca2728ab3e50c617d37fa99abf6f78613887
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-12-03 05:29:31 -0300

    feat(security): enhance token handling and security filter
    - Updated `SecurityFilter` to improve token validation and error handling.
    - Split token generation into `generateAccessToken` and `generateRefreshToken` in `TokenServiceImpl`.
    - Added `ApplicationConstants.AUTH_0` for consistent issuer reference.
    - Adjusted token expiration logic and added helper methods for expiration dates.

--- server/src/main/java/com/barataribeiro/medimanage/services/security/impl/TokenServiceImpl.java
+++ server/src/main/java/com/barataribeiro/medimanage/services/security/impl/TokenServiceImpl.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.barataribeiro.medimanage.constants.ApplicationConstants;
 import com.barataribeiro.medimanage.entities.models.User;
 import com.barataribeiro.medimanage.exceptions.MediManageException;
 import com.barataribeiro.medimanage.services.security.TokenService;
@@ -32,23 +33,48 @@ public class TokenServiceImpl implements TokenService {
     private String secretKey;
 
     @Override
-    public Map.Entry<String, Instant> generateToken(@NotNull User user, Boolean rememberMe) {
+    public Map.Entry<String, Instant> generateAccessToken(@NotNull User user) {
         Instant expirationDate;
         String token;
-        String tokenId = UUID.randomUUID().toString();
 
         try {
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
-            expirationDate = this.generateExpirationDateInDays(rememberMe != null && rememberMe ? 365 : 1);
+            expirationDate = this.generateExpirationDateOfFifteenMinutes();
 
             token = JWT.create()
-                    .withIssuer("auth0")
-                    .withSubject(user.getUsername())
-                    .withExpiresAt(expirationDate)
-                    .withClaim("role", user.getUserRoles().name())
-                    .withClaim("accountType", user.getAccountType().name())
-                    .withJWTId(tokenId)
-                    .sign(algorithm);
+                       .withIssuer(ApplicationConstants.AUTH_0)
+                       .withSubject(user.getUsername())
+                       .withClaim("role", user.getUserRoles().name())
+                       .withClaim("accountType", user.getAccountType().name())
+                       .withExpiresAt(expirationDate)
+                       .sign(algorithm);
+
+            return new AbstractMap.SimpleEntry<>(token, expirationDate);
+        } catch (IllegalArgumentException | JWTCreationException exception) {
+            log.atError().log(exception.getMessage());
+            throw new MediManageException();
+        }
+    }
+
+    @Override
+    public Map.Entry<String, Instant> generateRefreshToken(@NotNull User user, Boolean rememberMe) {
+        Instant expirationDate;
+        String token;
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            expirationDate = this.generateExpirationDateInDays(rememberMe != null && rememberMe ? 30 : 1);
+
+            String tokenId = UUID.randomUUID().toString();
+
+            token = JWT.create()
+                       .withIssuer(ApplicationConstants.AUTH_0)
+                       .withSubject(user.getUsername())
+                       .withExpiresAt(expirationDate)
+                       .withClaim("role", user.getUserRoles().name())
+                       .withClaim("accountType", user.getAccountType().name())
+                       .withJWTId(tokenId)
+                       .sign(algorithm);
 
             return new AbstractMap.SimpleEntry<>(token, expirationDate);
         } catch (IllegalArgumentException | JWTCreationException exception) {
@@ -61,13 +87,17 @@ public class TokenServiceImpl implements TokenService {
     public DecodedJWT validateToken(String token) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
-            JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth0").build();
+            JWTVerifier verifier = JWT.require(algorithm).withIssuer(ApplicationConstants.AUTH_0).build();
 
             return verifier.verify(token);
         } catch (JWTVerificationException exception) {
-            log.atError().log(exception.getMessage());
+            log.atError().log("Invalid token: {}", exception.getMessage());
             return null;
         }
+    }
+
+    private Instant generateExpirationDateOfFifteenMinutes() {
+        return LocalDateTime.now(ZoneOffset.UTC).plusMinutes(15).toInstant(ZoneOffset.UTC);
     }
 
     private Instant generateExpirationDateInDays(Integer days) {
