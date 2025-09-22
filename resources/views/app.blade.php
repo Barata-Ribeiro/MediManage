545a58f7dec5e33d125d8cbde2232ff086caba97
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-09-22 11:14:50 -0300

    fix(app.blade.php): update favicon links

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,35 +6,24 @@
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
-            (function() {
-                const appearance = '{{ $appearance ?? "system" }}';
+            (function () {
+                const appearance = '{{ $appearance ?? "system" }}'
 
-                if (appearance === 'system') {
-                    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                if (appearance === "system") {
+                    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches
 
                     if (prefersDark) {
-                        document.documentElement.classList.add('dark');
+                        document.documentElement.classList.add("dark")
                     }
                 }
-            })();
+            })()
         </script>
 
-        {{-- Inline style to set the HTML background color based on our theme in app.css --}}
-        <style>
-            html {
-                background-color: oklch(1 0 0);
-            }
+        <title inertia>{{ config('app.name', 'MediManage') }}</title>
 
-            html.dark {
-                background-color: oklch(0.145 0 0);
-            }
-        </style>
-
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="{{asset('images/favicon.ico')}}" sizes="any">
+        <link rel="icon" href="{{asset('images/favicon.svg')}}" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="{{asset('images/apple-touch-icon.png')}}">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
