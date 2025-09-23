07df6919f65327533e07843e3168748a3d56b70f
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-09-23 07:54:30 -0300

    feat(admin): add admin route for Super Admin access and response

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
+require __DIR__ . '/admin.php';
