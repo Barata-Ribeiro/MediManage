cf638decbed2661750290555906c6a6726f1a13d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-09-24 11:14:57 -0300

    fix(ui): adjust column visibility display and input styling

--- resources/js/components/data-table-view-options.tsx
+++ resources/js/components/data-table-view-options.tsx
@@ -37,7 +37,7 @@ export function DataTableViewOptions<TData>({
                                 checked={column.getIsVisible()}
                                 onCheckedChange={(value) => column.toggleVisibility(value)}
                             >
-                                {column.id}
+                                {column.id.replace(/[_-]+/g, ' ')}
                             </DropdownMenuCheckboxItem>
                         );
                     })}
