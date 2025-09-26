72ffd1cd456b2d711590ebb0eb45bb19c06afb58
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-09-26 07:27:37 -0300

    feat(toaster): add customizable toaster component with theme support

---
+++ resources/js/components/ui/sonner.tsx
@@ -0,0 +1,27 @@
+import { useTheme }                        from 'next-themes';
+import { Toaster as Sonner, ToasterProps } from 'sonner';
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme()
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+        } as React.CSSProperties
+      }
+      richColors
+      closeButton
+      duration={5000}
+      position="top-right"
+      {...props}
+    />
+  )
+}
+
+export { Toaster }
