558eaf5bc1c50e2c2f3492958b7f76c8ca05e9a2
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-09-30 06:19:52 -0300

    feat(notices): create notices table with user relationship and status fields

---
+++ database/migrations/2025_09_30_090928_create_notices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->string('title');
+            $table->text('description');
+            $table->enum('type', ['announcement', 'info', 'warning', 'alert'])->default('info');
+            $table->boolean('is_active')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notices');
+    }
+};
