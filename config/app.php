86e19f9a06d49fa8f60e1b7d87762c7f9e09aaa7
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-05 08:56:21 -0300

    feat(dependencies): add asika/agent package

--- config/app.php
+++ config/app.php
@@ -139,4 +139,14 @@ return [
     'admin_email' => env('APP_ADMIN_EMAIL', 'admin@example.com'),
     'admin_name' => env('APP_ADMIN_NAME', 'Administrator'),
     'admin_password' => env('APP_ADMIN_PASSWORD', '123456A#'),
+
+    /*|--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |--------------------------------------------------------------------------*/
+    Asika\Agent\AgentServiceProvider::class,
+    'Agent' => Asika\Agent\Facades\Agent::class,
 ];
