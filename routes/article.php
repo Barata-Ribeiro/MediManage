c97ac2a088774ba823aea0cdd370b30926441382
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-07 06:42:15 -0300

    refactor(routes): reorganize article and category routes for clarity and consistency

--- routes/article.php
+++ routes/article.php
@@ -1,16 +1,15 @@
 <?php
 
 use App\Http\Controllers\Article\ArticleController;
+use App\Http\Controllers\Article\CategoryController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified'])->prefix('manage')->group(function () {
     Route::prefix('articles')->group(function () {
         Route::get('/', [ArticleController::class, 'index'])->name('articles.index')->middleware('role:Super Admin|Manager');
-        Route::get('/my', [ArticleController::class, 'myArticles'])->name('articles.my')->middleware('permission:index.article');
+        Route::get('/my', [ArticleController::class, 'myIndex'])->name('articles.my')->middleware('permission:index.article');
 
         Route::get('/create', [ArticleController::class, 'create'])->name('articles.create')->middleware('permission:create.article');
         Route::post('/', [ArticleController::class, 'store'])->name('articles.store')->middleware('permission:create.article');
-
-        Route::get('/{article}', [ArticleController::class, 'show'])->name('articles.show')->middleware('permission:show.article');
 
         Route::get('/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit')->middleware('permission:edit.article');
         Route::patch('/{article}', [ArticleController::class, 'update'])->name('articles.update')->middleware('permission:edit.article');
@@ -19,12 +18,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
     });
 
     Route::prefix('categories')->group(function () {
-        Route::get('/', [ArticleController::class, 'indexCategories'])->name('categories.index')->middleware('permission:index.category');
-        Route::get('/create', [ArticleController::class, 'createCategory'])->name('categories.create')->middleware('permission:create.category');
-        Route::post('/', [ArticleController::class, 'storeCategory'])->name('categories.store')->middleware('permission:create.category');
-        Route::get('/{category}', [ArticleController::class, 'showCategory'])->name('categories.show')->middleware('permission:show.category');
-        Route::get('/{category}/edit', [ArticleController::class, 'editCategory'])->name('categories.edit')->middleware('permission:edit.category');
-        Route::patch('/{category}', [ArticleController::class, 'updateCategory'])->name('categories.update')->middleware('permission:edit.category');
-        Route::delete('/{category}', [ArticleController::class, 'destroyCategory'])->name('categories.destroy')->middleware('permission:destroy.category');
+        Route::get('/', [CategoryController::class, 'index'])->name('categories.index')->middleware('permission:index.category');
+        Route::get('/create', [CategoryController::class, 'create'])->name('categories.create')->middleware('permission:create.category');
+        Route::post('/', [CategoryController::class, 'store'])->name('categories.store')->middleware('permission:create.category');
+        Route::get('/{category}', [CategoryController::class, 'show'])->name('categories.show')->middleware('permission:show.category');
+        Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit')->middleware('permission:edit.category');
+        Route::patch('/{category}', [CategoryController::class, 'update'])->name('categories.update')->middleware('permission:edit.category');
+        Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy')->middleware('permission:destroy.category');
     });
 });
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,11 @@ use App\Http\Controllers\General\DashboardController;
 use App\Http\Controllers\General\PublicController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'guest'], function () {
-    Route::get('/', [PublicController::class, 'home'])->name('home');
-    Route::prefix('articles')->group(function () {
-        Route::get('/', [PublicController::class, 'articles'])->name('articles');
-        Route::get('/{article:slug}', [PublicController::class, 'article'])->name('article');
-    });
+Route::get('/', [PublicController::class, 'home'])->name('home')->middleware('guest');
+
+Route::prefix('articles')->group(function () {
+    Route::get('/', [PublicController::class, 'articles'])->name('articles');
+    Route::get('/{article:slug}', [PublicController::class, 'article'])->name('article');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
