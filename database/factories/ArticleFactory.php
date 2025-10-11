edf0c6450e59d7849d39f398a77d914780c8a203
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-11 09:02:50 -0300

    fix(article): adjust article factory with correct content generation and JSON formatting

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -19,17 +19,59 @@ class ArticleFactory extends Factory
     {
         $bgColor = fake()->hexColor();
         $thumbText = fake()->sentence(fake()->numberBetween(2, 5));
+        $content = fake()->paragraphs(3);
 
         return [
             'title' => fake()->sentence(),
             'subtitle' => fake()->sentence(),
             'slug' => fake()->unique()->slug(),
             'excerpt' => fake()->text(200),
-            'content_html' => '<p>' . implode('</p><p>', fake()->paragraphs(3)) . '</p>',
-            'content_json' => json_encode(['blocks' => array_map(fn($p) => ['type' => 'paragraph', 'data' => ['text' => $p]], fake()->paragraphs(3))]),
+            'content_html' => '<p>' . implode('</p><p>', $content) . '</p>',
+            'content_json' => $this->getFormattedJson($content),
             'thumbnail' => $this->fakeImageUrl(1920, 1080, $thumbText, $bgColor, 'FFFFFF', 'webp'),
             'is_published' => fake()->boolean(80),
         ];
+    }
+
+    /**
+     * Get formatted JSON content for the editor.
+     *
+     * @param array|string $content
+     * @return false|string
+     */
+    private function getFormattedJson(array|string $content): string|false
+    {
+        return json_encode([
+            'root' => [
+                'children' => array_map(function ($p) {
+                    return [
+                        'children' => [
+                            [
+                                'detail' => 0,
+                                'format' => 0,
+                                'mode' => 'normal',
+                                'style' => '',
+                                'text' => $p,
+                                'type' => 'text',
+                                'version' => 1
+                            ]
+                        ],
+                        'direction' => null,
+                        'format' => '',
+                        'indent' => 0,
+                        'type' => 'paragraph',
+                        'version' => 1,
+                        'textFormat' => 0,
+                        'textStyle' => ''
+                    ];
+                }, $content),
+                'direction' => null,
+                'format' => '',
+                'indent' => 0,
+                'type' => 'root',
+                'version' => 1
+            ]
+        ]);
     }
 
     private function fakeImageUrl(int $width, int $height, string $text, string $bg, string $color, string $format = 'jpeg'): string
