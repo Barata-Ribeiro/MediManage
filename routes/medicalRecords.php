80330b9eac71be21529bfd0d73f3eb56648430b0
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-12 07:00:07 -0300

    fix(medicalRecords): correct permission middleware for medical records index route

--- routes/medicalRecords.php
+++ routes/medicalRecords.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Medical\MedicalRecordController;
 Route::middleware(['auth'])->prefix('medical-records')->group(function () {
     Route::get('/patient-simple-search', [MedicalRecordController::class, 'patientSimpleSearch'])->name('medicalRecords.patientSimpleSearch')->middleware('role:Doctor');
 
-    Route::get('/', [MedicalRecordController::class, 'index'])->name('medicalRecords.index')->middleware('permission:medical_record.inex');
+    Route::get('/', [MedicalRecordController::class, 'index'])->name('medicalRecords.index')->middleware('permission:medical_record.index');
     Route::get('/create', [MedicalRecordController::class, 'create'])->name('medicalRecords.create')->middleware('permission:medical_record.create');
     Route::post('/', [MedicalRecordController::class, 'store'])->name('medicalRecords.store')->middleware('permission:medical_record.create');
     Route::get('/{medicalRecord}', [MedicalRecordController::class, 'show'])->name('medicalRecords.show')->middleware('permission:medical_record.show');
