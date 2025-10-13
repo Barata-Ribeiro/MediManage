7bdb185609b93004da6d38ce5ba3b21446685271
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-13 08:06:58 -0300

    feat(field): add reusable field components with variants for form handling

--- resources/js/components/ui/separator.tsx
+++ resources/js/components/ui/separator.tsx
@@ -1,14 +1,13 @@
-import * as React from "react"
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
-
-import { cn } from "@/lib/utils"
+import { cn }                  from '@/lib/utils';
+import * as SeparatorPrimitive from '@radix-ui/react-separator';
+import { ComponentProps }      from 'react';
 
 function Separator({
   className,
   orientation = "horizontal",
   decorative = true,
   ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}: ComponentProps<typeof SeparatorPrimitive.Root>) {
   return (
     <SeparatorPrimitive.Root
       data-slot="separator"
