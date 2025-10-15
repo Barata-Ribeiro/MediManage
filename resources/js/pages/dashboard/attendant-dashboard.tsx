7a6caba0c5c46ff6f621b7bf78fa0a488eb0896b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-15 06:30:25 -0300

    feat(attendant-dashboard): implement AttendantDashboard component for displaying attendant information and upcoming appointments

---
+++ resources/js/pages/dashboard/attendant-dashboard.tsx
@@ -0,0 +1,47 @@
+import DashboardHeader from '@/components/helpers/dashboard-header';
+import AppLayout from '@/layouts/app-layout';
+import DoctorUpcomingApptTable from '@/pages/dashboard/tables/doctor-upcoming-appt-table';
+import { dashboard } from '@/routes';
+import { BreadcrumbItem } from '@/types';
+import { PaginatedUpcomingAppointments } from '@/types/application/appointment';
+import { EmployeeInfo } from '@/types/application/employee';
+import { Head } from '@inertiajs/react';
+
+interface AttendantDashboardProps {
+    data: {
+        attendantInfo: EmployeeInfo;
+        upcomingToday: PaginatedUpcomingAppointments;
+    };
+}
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: dashboard().url,
+    },
+];
+
+export default function AttendantDashboard({ data }: Readonly<AttendantDashboardProps>) {
+    const { attendantInfo, upcomingToday } = data;
+
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Attendant Dashboard" />
+
+            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+                <DashboardHeader
+                    avatar={attendantInfo.user?.avatar}
+                    fullName={attendantInfo.full_name}
+                    email={attendantInfo.user?.email}
+                    phoneNumber={attendantInfo.phone_number}
+                    dateOfBirth={attendantInfo.date_of_birth}
+                    age={attendantInfo.age}
+                />
+
+                <div className="flex-1">
+                    <DoctorUpcomingApptTable pagination={upcomingToday} />
+                </div>
+            </div>
+        </AppLayout>
+    );
+}
