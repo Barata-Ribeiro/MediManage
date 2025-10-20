f03fe5c438aa3114bf09a1afdb471a1d43f4fca3
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-20 10:36:43 -0300

    feat(AppointmentService): implement getAppointmentsByDoctorWithRequest method to retrieve filtered appointments

--- app/Interfaces/AppointmentServiceInterface.php
+++ app/Interfaces/AppointmentServiceInterface.php
@@ -2,7 +2,21 @@
 
 namespace App\Interfaces;
 
+use App\Models\EmployeeInfo;
+use Illuminate\Http\Request;
+
 interface AppointmentServiceInterface
 {
-
+    /**
+     * Retrieve appointments for a specific doctor, applying filters from the HTTP request.
+     *
+     * This method returns the appointments associated with the given EmployeeInfo (doctor),
+     * applying filtering options specified in the Request object.
+     * Common request parameters are restricted to dates with time.
+     *
+     * @param \App\Models\EmployeeInfo|\EmployeeInfo $doctor The doctor (employee) whose appointments are requested.
+     * @param \Illuminate\Http\Request|\Request $request HTTP request containing filter, sort and pagination parameters.
+     * @return mixed The filtered list of appointments for the specified doctor.
+     */
+    public function getAppointmentsByDoctorWithRequest(EmployeeInfo $doctor, Request $request);
 }
--- app/Services/AppointmentService.php
+++ app/Services/AppointmentService.php
@@ -3,8 +3,28 @@
 namespace App\Services;
 
 use App\Interfaces\AppointmentServiceInterface;
+use App\Models\EmployeeInfo;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class AppointmentService implements AppointmentServiceInterface
 {
+    public function getAppointmentsByDoctorWithRequest(EmployeeInfo $doctor, Request $request)
+    {
+        $month = $request->query('month') ?? Carbon::now()->month;
+        $year = $request->query('year') ?? Carbon::now()->year;
 
+        $appointmentsQuery = $doctor->appointments();
+
+        $appointmentsQuery->whereYear('appointment_date', $year)
+            ->whereMonth('appointment_date', $month)
+            ->orderBy('appointment_date', 'asc');
+
+        $appointmentsQuery->with([
+            'patientInfo' => fn($q) => $q->select('id', 'user_id', 'first_name', 'last_name', 'date_of_birth'),
+            'patientInfo.user' => fn($q) => $q->select('id', 'name', 'email'),
+        ]);
+
+        return $appointmentsQuery->get();
+    }
 }
