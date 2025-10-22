0578ce689176883345154df12a54c83eb81599c1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-22 06:32:16 -0300

    fix(PatternSVG): add missing dark mode support for SVG patterns

--- resources/js/components/public/pattern-svg.tsx
+++ resources/js/components/public/pattern-svg.tsx
@@ -5,7 +5,7 @@ export default function PatternSVG(props: Readonly<SVGAttributes<SVGElement>>) {
         <svg
             {...props}
             aria-hidden
-            className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)] stroke-gray-200"
+            className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)] stroke-gray-200 dark:stroke-gray-500"
         >
             <defs>
                 <pattern
@@ -19,7 +19,7 @@ export default function PatternSVG(props: Readonly<SVGAttributes<SVGElement>>) {
                     <path d="M.5 200V.5H200" fill="none" />
                 </pattern>
             </defs>
-            <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+            <svg x="50%" y={-1} className="overflow-visible fill-gray-50 dark:fill-gray-700">
                 <path
                     d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
                     strokeWidth={0}
