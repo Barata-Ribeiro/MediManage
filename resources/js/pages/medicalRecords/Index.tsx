d049b2edfbaf67b6c06c6395579432753ffc8c1d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-23 05:44:57 -0300

    refactor(Index): reorganize layout and improve button integration for medical records

--- resources/js/pages/medicalRecords/Index.tsx
+++ resources/js/pages/medicalRecords/Index.tsx
@@ -1,11 +1,13 @@
 import medicalRecordController from '@/actions/App/Http/Controllers/Medical/MedicalRecordController';
 import { DataTable } from '@/components/data-table';
 import Heading from '@/components/heading';
+import { Button } from '@/components/ui/button';
 import Layout from '@/layouts/app-layout';
 import { column } from '@/pages/medicalRecords/column';
 import type { BreadcrumbItem } from '@/types';
 import { PaginationMedicalRecord } from '@/types/application/medicalRecord';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
+import { NotebookPenIcon } from 'lucide-react';
 
 export default function Index({ medicalRecords }: Readonly<{ medicalRecords: PaginationMedicalRecord }>) {
     const breadcrumbs: BreadcrumbItem[] = [
@@ -20,7 +22,15 @@ export default function Index({ medicalRecords }: Readonly<{ medicalRecords: Pag
             <Head title="Medical Records" />
 
             <div className="px-4 py-6">
-                <Heading title="Medical Records" description="Listing all medical records." />
+                <div className="flex items-center justify-between">
+                    <Heading title="Medical Records" description="Listing all medical records." />
+
+                    <Button variant="secondary" asChild>
+                        <Link href={medicalRecordController.create()} prefetch>
+                            <NotebookPenIcon aria-hidden /> Initiate Record
+                        </Link>
+                    </Button>
+                </div>
 
                 <section aria-label="Medical Records Table">
                     <DataTable columns={column} data={medicalRecords.data} pagination={medicalRecords} />
