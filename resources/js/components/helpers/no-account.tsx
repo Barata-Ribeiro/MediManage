cf3bb408e3eda0af68049eb97942f0670347cd00
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-24 07:56:19 -0300

    feat(NoAccount): enhance account handling with permission-based messaging

--- resources/js/components/helpers/no-account.tsx
+++ resources/js/components/helpers/no-account.tsx
@@ -2,7 +2,8 @@ import PatientAssociateAccountModal from '@/components/forms/patients/patient-as
 import { Button } from '@/components/ui/button';
 import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '@/components/ui/empty';
 import patient_info from '@/routes/patient_info';
-import { Link } from '@inertiajs/react';
+import { SharedData } from '@/types';
+import { Link, usePage } from '@inertiajs/react';
 import { UserRoundXIcon } from 'lucide-react';
 
 interface NoAccountProps {
@@ -10,6 +11,15 @@ interface NoAccountProps {
 }
 
 export default function NoAccount({ id }: Readonly<NoAccountProps>) {
+    const { auth } = usePage<SharedData>().props;
+
+    const hasPermissions = auth.permissions.includes('patient_info.create');
+
+    const description = {
+        true: 'No account found. An association is required to proceed. You can create one or associate an existing account.',
+        false: 'No account found. Please contact an attendant in the clinic for assistance.',
+    };
+
     return (
         <Empty>
             <EmptyHeader>
@@ -17,12 +27,9 @@ export default function NoAccount({ id }: Readonly<NoAccountProps>) {
                     <UserRoundXIcon aria-hidden />
                 </EmptyMedia>
                 <EmptyTitle>No Account</EmptyTitle>
-                <EmptyDescription>
-                    No account found. An association is required to proceed. You can create one or associate an existing
-                    account.
-                </EmptyDescription>
+                <EmptyDescription>{hasPermissions ? description.true : description.false}</EmptyDescription>
             </EmptyHeader>
-            {id && (
+            {id && hasPermissions && (
                 <EmptyContent className="flex-row flex-wrap justify-center">
                     <Button asChild>
                         <Link href={patient_info.newAccount(id)} as="button" prefetch="click">
