440690240974bb8d4b3fa483e660dbb45c99550d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-25 08:42:35 -0300

    feat(broadcasting): implement appointment channel authorization for attendants and doctors

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\Appointment;
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Auth;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('App.Models.Appointment.{id}', function ($user, $id) {
+    $userIsAttendantOrDoctor = $user->hasRole(['Attendant', 'Doctor']);
+    $appointmentExists = Appointment::whereId($id)->exists();
+    return $userIsAttendantOrDoctor && $appointmentExists;
 });
