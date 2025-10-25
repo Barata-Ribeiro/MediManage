5279b2f57021754e029037bbc978c1846994b8b1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-25 07:16:32 -0300

    feat(broadcasting): add Reverb configuration and setup for broadcasting events

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,9 +1,14 @@
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
+import { configureEcho } from '@laravel/echo-react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
+
+configureEcho({
+    broadcaster: 'reverb',
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
