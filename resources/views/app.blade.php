c9201f1a5b2a4f8ead836621dfc74235eda8f8bf
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-27 08:54:32 -0300

    refactor(app): add react scanner for local env

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,51 +1,57 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark'=> ($appearance ?? 'system') == 'dark'])>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- Inline script to detect system dark mode preference and apply it immediately --}}
-        <script>
-            (function() {
-                const appearance = '{{ $appearance ?? "system" }}';
+    {{-- React Scan for development environment only --}}
+    @env('local')
+    <script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js"></script>
+    @endenv
 
-                if (appearance === 'system') {
-                    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    {{-- Inline script to detect system dark mode preference and apply it immediately --}}
+    <script>
+        (function() {
+            const appearance = '{{ $appearance ?? "system" }}';
 
-                    if (prefersDark) {
-                        document.documentElement.classList.add('dark');
-                    }
+            if (appearance === 'system') {
+                const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+                if (prefersDark) {
+                    document.documentElement.classList.add('dark');
                 }
-            })();
-        </script>
-
-        {{-- Inline style to set the HTML background color based on our theme in app.css --}}
-        <style>
-            html {
-                background-color: oklch(0.9846 0.0017 247.8389);
             }
+        })();
 
-            html.dark {
-                background-color: oklch(0.1300 0.0280 261.6920);
-            }
-        </style>
+    </script>
 
-        <title inertia>{{ config('app.name', 'MediManage') }}</title>
+    {{-- Inline style to set the HTML background color based on our theme in app.css --}}
+    <style>
+        html {
+            background-color: oklch(0.9846 0.0017 247.8389);
+        }
 
-        <link rel="icon" href="{{asset('images/favicon.ico')}}" sizes="any">
-        <link rel="icon" href="{{asset('images/favicon.svg')}}" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="{{asset('images/apple-touch-icon.png')}}">
+        html.dark {
+            background-color: oklch(0.1300 0.0280 261.6920);
+        }
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
-              rel="stylesheet">
+    </style>
 
-        @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <title inertia>{{ config('app.name', 'MediManage') }}</title>
+
+    <link rel="icon" href="{{asset('images/favicon.ico')}}" sizes="any">
+    <link rel="icon" href="{{asset('images/favicon.svg')}}" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="{{asset('images/apple-touch-icon.png')}}">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+
+    @viteReactRefresh
+    @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+    @inertiaHead
+</head>
+<body class="font-sans antialiased">
+    @inertia
+</body>
 </html>
