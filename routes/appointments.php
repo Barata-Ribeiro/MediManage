b9608e345b3608b26cf8b23030489978cb69a414
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-28 09:18:33 -0300

    fix(appointment): update status route middleware to properly restrict access

--- routes/appointments.php
+++ routes/appointments.php
@@ -11,7 +11,7 @@ Route::middleware(['auth'])->prefix('appointments')->group(function () {
         Route::get('/', [AppointmentController::class, 'indexByPatient'])->name('appointments.patient.index')->middleware('role_or_permission:Patient|appointment.index');
     });
 
-    Route::patch('/{appointment}/status', [AppointmentController::class, 'updateStatus'])->name('appointments.update.status')->middleware('permission:appointment.edit');
+    Route::patch('/{appointment}/status', [AppointmentController::class, 'updateStatus'])->name('appointments.update.status')->middleware('role_or_permission:Doctor|appointment.edit');
 
     Route::get('/schedule', [AppointmentController::class, 'create'])->name('appointments.create')->middleware('permission:appointment.create');
     Route::post('/', [AppointmentController::class, 'store'])->name('appointments.store')->middleware('permission:appointment.create');
