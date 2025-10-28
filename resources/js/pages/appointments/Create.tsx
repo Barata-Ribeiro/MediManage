561ae09b901ec9a4feb8a29da9193ccd9010892f
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-28 05:46:02 -0300

    feat(appointment-create): replace form implementation with CreateAppointmentForm component

--- resources/js/pages/appointments/Create.tsx
+++ resources/js/pages/appointments/Create.tsx
@@ -1,12 +1,9 @@
-import CalendarSkeleton from '@/components/blocks/skeletons/calendar-skeleton';
+import CreateAppointmentForm from '@/components/forms/appointments/create-appointment-form';
 import Heading from '@/components/heading';
-import AppointmentCalendarPicker from '@/components/helpers/appointment-calendar-picker';
-import PatientSelectCombobox from '@/components/helpers/patient-select-combobox';
 import Layout from '@/layouts/app-layout';
 import appointments from '@/routes/appointments';
 import { BreadcrumbItem } from '@/types';
-import { Deferred, Form, Head } from '@inertiajs/react';
-import { useState } from 'react';
+import { Head } from '@inertiajs/react';
 
 interface CreateProps {
     occupiedSlots: Date[];
@@ -20,9 +17,6 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Create({ occupiedSlots }: Readonly<CreateProps>) {
-    const [finalDate, setFinalDate] = useState<string | undefined>(undefined);
-    const [patientId, setPatientId] = useState<string | null>(null);
-
     return (
         <Layout breadcrumbs={breadcrumbs}>
             <Head title="Schedule Appointment" />
@@ -34,41 +28,7 @@ export default function Create({ occupiedSlots }: Readonly<CreateProps>) {
                 />
 
                 <section className="rounded-md bg-card p-4 shadow sm:p-6">
-                    <Form
-                        {...appointments.store.form()}
-                        options={{ preserveScroll: true }}
-                        className="space-y-6 inert:pointer-events-none inert:opacity-50"
-                        transform={(data) => ({
-                            ...data,
-                            appointment_date: finalDate,
-                            patient_info_id: patientId,
-                        })}
-                        disableWhileProcessing
-                    >
-                        {({ errors }) => (
-                            <>
-                                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-                                    <PatientSelectCombobox
-                                        setPatientId={setPatientId}
-                                        error={errors.patient_info_id}
-                                        size="full"
-                                    />
-                                    {/* TODO: Implement doctor select here */}
-                                </div>
-
-                                <div className="*:mx-auto">
-                                    <Deferred data="occupiedSlots" fallback={<CalendarSkeleton />}>
-                                        <AppointmentCalendarPicker
-                                            occupiedSlots={occupiedSlots}
-                                            setFinalDate={setFinalDate}
-                                        />
-                                    </Deferred>
-                                </div>
-
-                                <pre>{JSON.stringify(finalDate, null, 2)}</pre>
-                            </>
-                        )}
-                    </Form>
+                    <CreateAppointmentForm occupiedSlots={occupiedSlots} />
                 </section>
             </div>
         </Layout>
