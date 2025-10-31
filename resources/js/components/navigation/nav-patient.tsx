fe18170faa12fb8b197b9efd21ad6568d9f1f499
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-31 06:07:19 -0300

    fix(nav-patient): update appointment URL and refine patient check logic

--- resources/js/components/navigation/nav-patient.tsx
+++ resources/js/components/navigation/nav-patient.tsx
@@ -5,6 +5,7 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
+import appointments from '@/routes/appointments';
 import { NavItem, SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 import { ClipboardClockIcon, ClipboardListIcon, PillIcon } from 'lucide-react';
@@ -14,15 +15,14 @@ export function NavPatient() {
 
     const auth = props.auth;
 
-    // TODO: Check for 'patient_info_id' when route for user to register a patient info is available
-    const isPatient = auth.roles.includes('Patient');
+    const isPatient = auth.roles.includes('Patient') && !!auth.user.patient_info_id;
     if (!isPatient) return null;
 
     // TODO: Replace placeholder URLs with actual routes when available
     const patientItems: NavItem[] = [
         {
             title: 'My Appointments',
-            href: 'placeholder_for_appointments_url',
+            href: appointments.patient.index(auth.user.patient_info_id!).url,
             icon: ClipboardClockIcon,
         },
         {
@@ -41,22 +41,23 @@ export function NavPatient() {
         <SidebarGroup className="px-2 py-0">
             <SidebarGroupLabel>Patient</SidebarGroupLabel>
             <SidebarMenu>
-                {patientItems.map((item) => {
-                    const urlWithoutQuery = url.split('?')[0];
-                    const endingPath = typeof item.href === 'string' ? item.href : item.href.url;
-                    const isActive = urlWithoutQuery.endsWith(endingPath);
+                {isPatient &&
+                    patientItems.map((item) => {
+                        const urlWithoutQuery = url.split('?')[0];
+                        const endingPath = typeof item.href === 'string' ? item.href : item.href.url;
+                        const isActive = urlWithoutQuery.endsWith(endingPath);
 
-                    return (
-                        <SidebarMenuItem key={item.title}>
-                            <SidebarMenuButton asChild isActive={isActive} tooltip={{ children: item.title }}>
-                                <Link href={item.href} prefetch>
-                                    {item.icon && <item.icon />}
-                                    <span>{item.title}</span>
-                                </Link>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                    );
-                })}
+                        return (
+                            <SidebarMenuItem key={item.title}>
+                                <SidebarMenuButton asChild isActive={isActive} tooltip={{ children: item.title }}>
+                                    <Link href={item.href} prefetch>
+                                        {item.icon && <item.icon />}
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        );
+                    })}
             </SidebarMenu>
         </SidebarGroup>
     );
