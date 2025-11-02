7b390cb6edb59b895f7e84e9774c4cce5b26faf1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-02 09:05:32 -0300

    refactor(app-public-layout): remove unused imports and simplify NavIndex props

--- resources/js/layouts/app/app-public-layout.tsx
+++ resources/js/layouts/app/app-public-layout.tsx
@@ -1,26 +1,17 @@
-import AppNotice from '@/components/app-notice';
 import ScrollToTop from '@/components/helpers/scroll-to-top';
 import Footer from '@/components/public/footer';
 import NavIndex from '@/components/public/nav-index';
-import { SharedData } from '@/types';
-import { Article } from '@/types/application/article';
-import { usePage } from '@inertiajs/react';
 import { ReactNode } from 'react';
 
-type LatestArticles = Pick<Article, 'id' | 'title' | 'slug' | 'created_at'>[];
-
 export default function AppPublicLayout({ children }: Readonly<{ children: ReactNode }>) {
-    const { auth, latestArticles, notices } = usePage<SharedData>().props;
-
     return (
         <div className="bg-background">
-            <NavIndex articles={latestArticles as LatestArticles} auth={auth} />
+            <NavIndex />
 
             <main>{children}</main>
 
             <Footer />
             <ScrollToTop />
-            <AppNotice notices={notices} />
         </div>
     );
 }
