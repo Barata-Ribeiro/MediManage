f75ab35aaafb78a7eff7ba5ec8f18499b3b9df81
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-03 14:03:03 -0300

    refactor(DashboardController): streamline constructor and add manager dashboard handling

--- app/Http/Controllers/General/DashboardController.php
+++ app/Http/Controllers/General/DashboardController.php
@@ -6,28 +6,25 @@ use App\Http\Controllers\Controller;
 use App\Services\DashboardAdminService;
 use App\Services\DashboardAttendantService;
 use App\Services\DashboardDoctorDoctorService;
+use App\Services\DashboardManagerService;
 use App\Services\DashboardUserService;
 use Auth;
 use Inertia\Inertia;
-use Log;
 
 class DashboardController extends Controller
 {
-    private DashboardDoctorDoctorService $dashboardDoctorService;
-    private DashboardAdminService $dashboardAdminService;
-    private DashboardAttendantService $dashboardAttendantService;
-    private DashboardUserService $dashboardUserService;
-
     public function __construct(
-        DashboardDoctorDoctorService $dashboardDoctorService,
-        DashboardAdminService $dashboardAdminService,
-        DashboardAttendantService $dashboardAttendantService,
-        DashboardUserService $dashboardUserService
+        private DashboardDoctorDoctorService $dashboardDoctorService,
+        private DashboardAdminService $dashboardAdminService,
+        private DashboardAttendantService $dashboardAttendantService,
+        private DashboardUserService $dashboardUserService,
+        private DashboardManagerService $dashboardManagerService,
     ) {
         $this->dashboardDoctorService = $dashboardDoctorService;
         $this->dashboardAdminService = $dashboardAdminService;
         $this->dashboardAttendantService = $dashboardAttendantService;
         $this->dashboardUserService = $dashboardUserService;
+        $this->dashboardManagerService = $dashboardManagerService;
     }
 
     /**
@@ -42,16 +39,23 @@ class DashboardController extends Controller
             case 'Super Admin':
             case 'Admin':
                 $data = $this->dashboardAdminService->getAdminDashboardData();
+
                 return Inertia::render('dashboard/admin-dashboard', $data);
             case 'Doctor':
                 $data = $this->dashboardDoctorService->getDoctorDashboardData();
+
                 return Inertia::render('dashboard/doctor-dashboard', $data);
             case 'Attendant':
                 $data = $this->dashboardAttendantService->getAttendantDashboardData();
+
                 return Inertia::render('dashboard/attendant-dashboard', $data);
+            case 'Manager':
+                $data = $this->dashboardManagerService->getManagerDashboardData();
+
+                return Inertia::render('dashboard/manager-dashboard', $data);
             default:
                 $data = $this->dashboardUserService->getUserDashboardData();
-                Log::debug('User Dashboard Data: ', $data);
+
                 return Inertia::render('dashboard/user-dashboard', $data);
         }
     }
