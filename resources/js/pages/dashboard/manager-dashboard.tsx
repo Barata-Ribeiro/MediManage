638e8e6a9ab0cacc5c9f531c566b42d3c2efeabe
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-04 05:50:01 -0300

    feat(ManagerDashboard): define interfaces for contracts, invoices, and employee payments data

--- resources/js/pages/dashboard/manager-dashboard.tsx
+++ resources/js/pages/dashboard/manager-dashboard.tsx
@@ -1,13 +1,46 @@
+import NewPatientsByMonthChart from '@/components/charts/new-patients-by-month-chart';
+import DashboardHeader from '@/components/helpers/dashboard-header';
 import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
-import { BreadcrumbItem } from '@/types';
+import { BreadcrumbItem, ChartItem } from '@/types';
 import { EmployeeInfo } from '@/types/application/employee';
 import { Head } from '@inertiajs/react';
+
+export interface TotalPatients {
+    total: number;
+    percentage_change: number;
+}
+
+export interface ContractsData {
+    total_contracts: number;
+    active_contracts: number;
+    contracts_by_type: ChartItem;
+    total_earnings_current_month: number;
+}
+
+export interface InvoicesData {
+    total_invoices: number;
+    total_amount: number;
+    pending_invoices: number;
+    paid_invoices: number;
+    pending_amount: number;
+}
+
+export interface EmployeePaymentsData {
+    total_payments: number;
+    total_amount_paid: number;
+    payments_by_month: ChartItem;
+}
 
 interface ManagerDashboardProps {
     data: {
         managerInfo: EmployeeInfo;
-        [key: string]: unknown;
+        employeesByPosition: ChartItem;
+        totalPatients: TotalPatients;
+        newPatientsByMonth: ChartItem;
+        contractsData: ContractsData;
+        invoicesData: InvoicesData;
+        employeePaymentsData: EmployeePaymentsData;
     };
 }
 
@@ -26,6 +59,22 @@ export default function ManagerDashboard({ data }: Readonly<ManagerDashboardProp
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Manager Dashboard" />
+            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+                <DashboardHeader
+                    avatar={data.managerInfo.user?.avatar}
+                    fullName={data.managerInfo.full_name}
+                    email={data.managerInfo.user?.email}
+                    phoneNumber={data.managerInfo.phone_number}
+                    dateOfBirth={data.managerInfo.date_of_birth}
+                    age={data.managerInfo.age}
+                />
+
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <NewPatientsByMonthChart chartData={data.newPatientsByMonth} />
+
+                    {/* TODO: Implement other charts and data displays for contracts, invoices, and employee payments */}
+                </div>
+            </div>
         </AppLayout>
     );
 }
