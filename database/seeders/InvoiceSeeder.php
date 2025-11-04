25d9f1b913cd4d249dca8dbfb5b339fa953764d3
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-04 07:19:30 -0300

    fix(InvoiceSeeder): update consultation date and due date handling for invoices

--- database/seeders/InvoiceSeeder.php
+++ database/seeders/InvoiceSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Appointment;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class InvoiceSeeder extends Seeder
@@ -15,11 +16,13 @@ class InvoiceSeeder extends Seeder
         $completedAppointments = Appointment::whereStatus('completed')->with('patientInfo')->get();
 
         foreach ($completedAppointments as $appointment) {
+            $appointmentDate = Carbon::parse($appointment->appointment_date);
+
             $appointment->patientInfo->invoices()->create([
-                'consultation_date' => $appointment->appointment_date,
+                'consultation_date' => $appointmentDate,
                 'notes' => 'Invoice for appointment ID: '.$appointment->id.' with Dr. '.$appointment->employeeInfo->full_name,
-                'due_date' => now()->addDays(30),
-                'amount' => fake()->randomFloat(2, 100, 500), // Random amount for demo purposes
+                'due_date' => $appointmentDate->addDays(30),
+                'amount' => fake()->randomFloat(2, 400, 600), // Random amount for demo purposes
                 'payment_method' => fake()->randomElement(['credit_card', 'cash', 'insurance']),
                 'status' => 'paid',
             ]);
