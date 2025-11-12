f1827d91dac5bde5c5876d3c597c4861d2376a09
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-12 05:50:17 -0300

    fix(types): update created_at and updated_at types to Date for Notice interface

--- resources/js/types/application/notice.d.ts
+++ resources/js/types/application/notice.d.ts
@@ -1,17 +1,24 @@
 import { User } from '@/types/admin/users';
 import { NoticeType } from '@/types/application/enums';
+import { PaginationMeta } from '@/types/index';
 
 export interface Notice {
     id: number;
     user_id: number;
+
     title: string;
     description: string;
     type: NoticeType;
     is_active: number;
-    created_at: string;
-    updated_at: string;
+
+    created_at: Date;
+    updated_at: Date;
 }
 
-interface NoticeWithRelations extends Notice {
+export interface NoticeWithRelations extends Notice {
     user: User;
 }
+
+export type TableNotice = Omit<NoticeWithRelations, 'description'>;
+
+export type PaginationNotice = PaginationMeta<TableNotice[]>;
