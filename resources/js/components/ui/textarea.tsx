e4f10432ce75328b8f15850f1ded0d1d51e76773
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-11-15 09:37:52 -0300

    chore(ui): install button group and input group component

--- resources/js/components/ui/textarea.tsx
+++ resources/js/components/ui/textarea.tsx
@@ -1,11 +1,15 @@
-import { cn }     from '@/lib/utils';
 import * as React from 'react';
 
-function Textarea({ className, ...props }: Readonly<React.ComponentProps<'textarea'>>) {
+import { cn } from '@/lib/utils';
+
+function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
     return (
         <textarea
             data-slot="textarea"
-            className={cn('flex field-sizing-content dark:placeholder:text-muted-foreground/80 min-h-16 w-full rounded-md border border-input bg-input px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none placeholder:text-muted-foreground/50 focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:aria-invalid:ring-destructive/40', className)}
+            className={cn(
+                'flex field-sizing-content min-h-16 w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:ring-destructive/40',
+                className,
+            )}
             {...props}
         />
     );
